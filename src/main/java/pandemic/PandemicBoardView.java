c9fcc2dcca6c04fe9f076ceff142415320f7b3db
Raluca D. Gaina <r.d.gaina@qmul.ac.uk>
2020-04-09 14:16:13 +0100

    No more javafx

--- src/main/java/pandemic/PandemicBoardView.java
+++ src/main/java/pandemic/PandemicBoardView.java
@@ -4,7 +4,6 @@ import components.*;
 import content.*;
 import core.Game;
 import core.GameState;
-import javafx.util.Pair;
 import utilities.Hash;
 import utilities.ImageIO;
 import utilities.Utils;
@@ -12,6 +11,7 @@ import utilities.Vector2D;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Point2D;
 import java.util.ArrayList;
 
 import static pandemic.PandemicCardView.drawCard;
@@ -32,34 +32,34 @@ public class PandemicBoardView extends JComponent {
     int diseaseCubeDistance = 2;
     int counterWidth = 20, counterHeight = 20;
 
-    Pair<Integer, Integer>[] infectionPositions = new Pair[]{
-            new Pair<>(755, 180),
-            new Pair<>(795, 180),
-            new Pair<>(835, 180),
-            new Pair<>(875, 180),
-            new Pair<>(915, 180),
-            new Pair<>(955, 180),
-            new Pair<>(995, 180)
+    Point2D[] infectionPositions = new Point2D[]{
+            new Point2D.Double(755, 180),
+            new Point2D.Double(795, 180),
+            new Point2D.Double(835, 180),
+            new Point2D.Double(875, 180),
+            new Point2D.Double(915, 180),
+            new Point2D.Double(955, 180),
+            new Point2D.Double(995, 180)
     };
-    Pair<Integer, Integer>[] outbreakPositions = new Pair[]{
-            new Pair<>(75, 450),
-            new Pair<>(120, 495),
-            new Pair<>(75, 530),
-            new Pair<>(120, 565),
-            new Pair<>(75, 600),
-            new Pair<>(120, 630),
-            new Pair<>(75, 665),
-            new Pair<>(120, 700),
-            new Pair<>(75, 730)
+    Point2D[] outbreakPositions = new Point2D[]{
+            new Point2D.Double(75, 450),
+            new Point2D.Double(120, 495),
+            new Point2D.Double(75, 530),
+            new Point2D.Double(120, 565),
+            new Point2D.Double(75, 600),
+            new Point2D.Double(120, 630),
+            new Point2D.Double(75, 665),
+            new Point2D.Double(120, 700),
+            new Point2D.Double(75, 730)
     };
 
-    Pair<Integer, Integer> infectionDiscardPosition = new Pair<>(915, 50);
-    Pair<Integer, Integer> playerDiscardPosition = new Pair<>(880, 625);
-    Pair<Integer, Integer>[] diseaseMarkerPositions = new Pair[]{
-            new Pair<>(395, 775),
-            new Pair<>(450, 775),
-            new Pair<>(510, 775),
-            new Pair<>(560, 775)
+    Point2D infectionDiscardPosition = new Point2D.Double(915, 50);
+    Point2D playerDiscardPosition = new Point2D.Double(880, 625);
+    Point2D[] diseaseMarkerPositions = new Point2D[]{
+            new Point2D.Double(395, 775),
+            new Point2D.Double(450, 775),
+            new Point2D.Double(510, 775),
+            new Point2D.Double(560, 775)
     };
 
     public PandemicBoardView(Game g, String backgroundPath) {
@@ -136,27 +136,27 @@ public class PandemicBoardView extends JComponent {
 
         // Draw infection rate marker
         Counter ir = game.findCounter("Infection Rate");
-        Pair<Integer, Integer> pos = infectionPositions[ir.getValue()];
-        g.drawImage(ImageIO.GetInstance().getImage("data/infectionRate.png"), pos.getKey(), pos.getValue(), null, null);
+        Point2D pos = infectionPositions[ir.getValue()];
+        g.drawImage(ImageIO.GetInstance().getImage("data/infectionRate.png"), (int)pos.getX(), (int)pos.getY(), null, null);
 
         // Draw outbreak marker
         Counter ob = game.findCounter("Outbreaks");
         pos = outbreakPositions[ob.getValue()];
-        g.drawImage(ImageIO.GetInstance().getImage("data/outbreakMarker.png"), pos.getKey(), pos.getValue(), null, null);
+        g.drawImage(ImageIO.GetInstance().getImage("data/outbreakMarker.png"), (int)pos.getX(), (int)pos.getY(), null, null);
 
         // Discard piles
         Deck pDiscard = game.findDeck("Player Deck Discard");
         if (pDiscard != null) {
             Card cP = pDiscard.draw();
             if (cP != null) {
-                drawCard(g, 100, 50, cP, null, playerDiscardPosition.getKey(), playerDiscardPosition.getValue());
+                drawCard(g, 100, 50, cP, null, (int)playerDiscardPosition.getX(), (int)playerDiscardPosition.getY());
             }
         }
         Deck iDiscard = game.findDeck("Infection Discard");
         if (iDiscard != null) {
             Card cI = iDiscard.draw();
             if (cI != null) {
-                drawCard(g, 100, 50, cI, null, infectionDiscardPosition.getKey(), infectionDiscardPosition.getValue());
+                drawCard(g, 100, 50, cI, null, (int)infectionDiscardPosition.getX(), (int)infectionDiscardPosition.getY());
             }
         }
 
@@ -174,12 +174,12 @@ public class PandemicBoardView extends JComponent {
 
     private void drawCounter(Graphics2D g, int value, Color color, int idx) {
         if (value > 0) {
-            Pair<Integer, Integer> pos = diseaseMarkerPositions[idx];
+            Point2D pos = diseaseMarkerPositions[idx];
             g.setColor(color);
-            g.fillOval(pos.getKey(), pos.getValue(), counterWidth, counterHeight);
+            g.fillOval((int)pos.getX(), (int)pos.getY(), counterWidth, counterHeight);
             if (value == 2) {
                 g.setColor(Color.white);
-                g.drawLine(pos.getKey(), pos.getValue(), pos.getKey() + counterWidth, pos.getValue() + counterHeight);
+                g.drawLine((int)pos.getX(), (int)pos.getY(), (int)pos.getX() + counterWidth, (int)pos.getY() + counterHeight);
             }
             g.setColor(Color.black);
         }
