0949b0caf620542aae5815324bfa025ae87abf91
Raluca D. Gaina <r.d.gaina@qmul.ac.uk>
2020-05-31 11:37:51 +0100

    Generic actions generified further

--- src/main/java/core/actions/RemoveComponentFromDeck.java
+++ src/main/java/core/actions/RemoveComponentFromDeck.java
@@ -1,26 +1,25 @@
 package core.actions;
 
 import core.AbstractGameState;
-import core.components.Card;
+import core.components.Component;
 import core.components.Deck;
 
 import java.util.Objects;
 
 @SuppressWarnings("unchecked")
-public class RemoveCardWithCard extends DrawCard {
+public class RemoveComponentFromDeck<T extends Component> extends DrawCard {
     private int deck;
-    private int removeCard;
+    private int componentIdx;
 
-    public RemoveCardWithCard(int deckFrom, int deckTo, int fromIndex, int deckRemove, int cardRemove) {
+    public RemoveComponentFromDeck(int deckFrom, int deckTo, int fromIndex, int deckRemove, int componentRemove) {
         super(deckFrom, deckTo, fromIndex);
         this.deck = deckRemove;
-        this.removeCard = cardRemove;
+        this.componentIdx = componentRemove;
     }
-
 
     @Override
     public boolean execute(AbstractGameState gs) {
-        ((Deck<Card>)gs.getComponentById(deck)).remove(removeCard); // card removed from the game
+        ((Deck<T>)gs.getComponentById(deck)).remove(componentIdx); // card removed from the game
 
         // Discard other card from player hand
         return super.execute(gs);
@@ -31,35 +30,35 @@ public class RemoveCardWithCard extends DrawCard {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         if (!super.equals(o)) return false;
-        RemoveCardWithCard that = (RemoveCardWithCard) o;
+        RemoveComponentFromDeck that = (RemoveComponentFromDeck) o;
         return deck == that.deck &&
-                removeCard == that.removeCard;
+                componentIdx == that.componentIdx;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), deck, removeCard);
+        return Objects.hash(super.hashCode(), deck, componentIdx);
     }
 
     @Override
     public String toString() {
-        return "RemoveCardWithCard{" +
+        return "RemoveComponentFromDeck{" +
                 "deck=" + deck +
-                ", removeCard=" + removeCard +
+                ", component=" + componentIdx +
                 '}';
     }
 
     @Override
     public String getString(AbstractGameState gameState) {
-        return "RemoveCardWithCard{" +
+        return "RemoveComponentFromDeck{" +
                 "deck=" + deck +
-                ", removeCard=" + removeCard +
+                ", component=" + componentIdx +
                 ", cardPlayed=" + getCard(gameState).getComponentName() +
                 '}';
     }
 
-    public int getRemoveCard() {
-        return removeCard;
+    public int getComponent() {
+        return componentIdx;
     }
 
     public int getDeck() {
