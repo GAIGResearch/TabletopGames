36362b8534a9cfdd24245d2cc86882e233069f41
Raluca D. Gaina <r.d.gaina@qmul.ac.uk>
2020-06-02 14:55:38 +0100

    Documentation and reordering

--- src/main/java/core/actions/AbstractAction.java
+++ src/main/java/core/actions/AbstractAction.java
@@ -7,23 +7,16 @@ import core.interfaces.IPrintable;
 public abstract class AbstractAction implements IPrintable {
 
     /**
-     * Executes this action, applying its effect to the given game state.
+     * Executes this action, applying its effect to the given game state. Can access any component IDs stored
+     * through the AbstractGameState.getComponentById(int id) method.
      * @param gs - game state which should be modified by this action.
      * @return - true if successfully executed, false otherwise.
      */
     public abstract boolean execute(AbstractGameState gs);
 
     /**
-     * Returns the card used to play this action. Null if no card was needed (as default).
-     * @return - Card, to be discarded.
-     */
-    public Card getCard(AbstractGameState gs) {
-        return null;
-    }
-
-    /**
      * Create a copy of this action, with all of its variables.
-     * NO REFERENCES TO OBJECTS TO BE KEPT IN ACTIONS, PRIMITIVE TYPES ONLY.
+     * NO REFERENCES TO COMPONENTS TO BE KEPT IN ACTIONS, PRIMITIVE TYPES ONLY.
      * @return - new AbstractAction object with the same properties.
      */
     public abstract AbstractAction copy();
@@ -36,4 +29,12 @@ public abstract class AbstractAction implements IPrintable {
 
     @Override
     public abstract String getString(AbstractGameState gameState);
+
+    /**
+     * Returns the card used to play this action. Null if no card was needed (as default).
+     * @return - Card, to be discarded.
+     */
+    public Card getCard(AbstractGameState gs) {
+        return null;
+    }
 }
