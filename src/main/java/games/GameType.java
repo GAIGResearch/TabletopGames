9155bb01d38bd0b7b94c79de4d77ab40c9a61ef5
Raluca D. Gaina <r.d.gaina@qmul.ac.uk>
2020-06-02 17:31:17 +0100

    Update GameType.java
    Documentation and moving methods to be modified to the top of the class.

--- src/main/java/games/GameType.java
+++ src/main/java/games/GameType.java
@@ -34,6 +34,11 @@ import static games.GameType.Mechanic.*;
  */
 public enum GameType {
 
+    /**
+     * Each game in the framework corresponds to a enum value here, giving minimum players, maximum players,
+     * a list of categories the game belongs to, and a list of mechanics the game uses.
+     * Add here all games, planned or implemented.
+     */
     Pandemic(2, 4,
             new ArrayList<Category>() {{ add(Strategy); add(Medical); }},
             new ArrayList<Mechanic>() {{ add(ActionPoints); add(Cooperative); add(HandManagement);
@@ -61,6 +66,113 @@ public enum GameType {
     Carcassonne (2, 5,
             new ArrayList<Category>() {{ add(Strategy); add(CityBuilding); add(Medieval); add(TerritoryBuilding); }},
             new ArrayList<Mechanic>() {{ add(Influence); add(MapAddition); add(TilePlacement); }});
+
+    /**
+     * Converts a given string to the enum type corresponding to the game.
+     * Add here all games, planned or implemented.
+     * @param game - string of a game type
+     * @return - GameType corresponding to String
+     */
+    public GameType stringToGameType(String game) {
+        switch (game.toLowerCase()) {
+            case "pandemic":
+                return Pandemic;
+            case "tictactoe":
+                return TicTacToe;
+            case "explodingkittens":
+                return ExplodingKittens;
+            case "loveletter":
+                return LoveLetter;
+            case "uno":
+                return Uno;
+            case "virus":
+                return Virus;
+            case "coltexpress":
+                return ColtExpress;
+            case "carcassonne":
+                return Carcassonne;
+        }
+        System.out.println("Game type not found, returning null. ");
+        return null;
+    }
+
+    /**
+     * Creates an instance of the given game type, with a specific number of players and game seed.
+     * Add here all games implemented.
+     * @param nPlayers - number of players taking part in the game, used for initialisation.
+     * @param seed - seed for this game.
+     * @return - instance of Game object; null if game not implemented.
+     */
+    public Game createGameInstance(int nPlayers, long seed) {
+        if (nPlayers < minPlayers || nPlayers > maxPlayers) {
+            System.out.println("Unsupported number of players: " + nPlayers
+                    + ". Should be in range [" + minPlayers + "," + maxPlayers + "].");
+            return null;
+        }
+
+        AbstractGameParameters params;
+        AbstractForwardModel forwardModel = null;
+        AbstractGameState gameState = null;
+
+        switch(this) {
+            case Pandemic:
+                params = new PandemicParameters("data/pandemic/", seed);
+                forwardModel = new PandemicForwardModel(params, nPlayers);
+                gameState = new PandemicGameState(params, nPlayers);
+                break;
+            case TicTacToe:
+                params = new TicTacToeGameParameters(seed);
+                forwardModel = new TicTacToeForwardModel();
+                gameState = new TicTacToeGameState(params, nPlayers);
+                break;
+            case ExplodingKittens:
+                params = new ExplodingKittenParameters(seed);
+                forwardModel = new ExplodingKittensForwardModel();
+                gameState = new ExplodingKittensGameState(params, nPlayers);
+                break;
+            case LoveLetter:
+                params = new LoveLetterParameters(seed);
+                forwardModel = new LoveLetterForwardModel();
+                gameState = new LoveLetterGameState(params, nPlayers);
+                break;
+            case Uno:
+                params = new UnoGameParameters(seed);
+                forwardModel = new UnoForwardModel();
+                gameState = new UnoGameState(params, nPlayers);
+                break;
+            case Virus:
+                params = new VirusGameParameters(seed);
+                forwardModel = new VirusForwardModel();
+                gameState = new VirusGameState(params, nPlayers);
+                break;
+        }
+
+        if (forwardModel != null) {
+            return new Game(this, forwardModel, gameState);
+        }
+        return null;
+    }
+
+    /**
+     * Creates a graphical user interface for the given game type. Add here all games with a GUI available.
+     * @param gameState - initial game state from the game.
+     * @param ac - ActionController object allowing for user interaction with the GUI.
+     * @return - GUI for the given game type.
+     */
+    @SuppressWarnings("SwitchStatementWithTooFewBranches")
+    public AbstractGUI createGUI(AbstractGameState gameState, ActionController ac) {
+
+        AbstractGUI gui = null;
+
+        switch(this) {
+            case Pandemic:
+                gui = new PandemicGUI(gameState, ac);
+                break;
+        }
+
+        return gui;
+    }
+
 
     // Minimum and maximum number of players supported in this game
     private int minPlayers, maxPlayers;
@@ -192,115 +304,11 @@ public enum GameType {
     }
 
     /**
-     * Converts a given string to the enum type corresponding to the game.
-     * @param game - string of a game type
-     * @return - GameType corresponding to String
-     */
-    public GameType stringToGameType(String game) {
-        switch (game.toLowerCase()) {
-            case "pandemic":
-                return Pandemic;
-            case "tictactoe":
-                return TicTacToe;
-            case "explodingkittens":
-                return ExplodingKittens;
-            case "loveletter":
-                return LoveLetter;
-            case "uno":
-                return Uno;
-            case "virus":
-                return Virus;
-            case "coltexpress":
-                return ColtExpress;
-            case "carcassonne":
-                return Carcassonne;
-        }
-        System.out.println("Game type not found, returning null. ");
-        return null;
-    }
-
-    /**
      * Creates an instance of the given game type with nPlayers number of players and a new random seed.
      * @param nPlayers - number of players for the game.
      * @return - instance of Game object; null if game not implemented.
      */
     public Game createGameInstance(int nPlayers) {
         return createGameInstance(nPlayers, System.currentTimeMillis());
-    }
-
-    /**
-     * Creates an instance of the given game type, with a specific number of players and game seed.
-     * @param nPlayers - number of players taking part in the game, used for initialisation.
-     * @param seed - seed for this game.
-     * @return - instance of Game object; null if game not implemented.
-     */
-    public Game createGameInstance(int nPlayers, long seed) {
-        if (nPlayers < minPlayers || nPlayers > maxPlayers) {
-            System.out.println("Unsupported number of players: " + nPlayers
-                    + ". Should be in range [" + minPlayers + "," + maxPlayers + "].");
-            return null;
-        }
-
-        AbstractGameParameters params;
-        AbstractForwardModel forwardModel = null;
-        AbstractGameState gameState = null;
-
-        switch(this) {
-            case Pandemic:
-                params = new PandemicParameters("data/pandemic/", seed);
-                forwardModel = new PandemicForwardModel(params, nPlayers);
-                gameState = new PandemicGameState(params, nPlayers);
-                break;
-            case TicTacToe:
-                params = new TicTacToeGameParameters(seed);
-                forwardModel = new TicTacToeForwardModel();
-                gameState = new TicTacToeGameState(params, nPlayers);
-                break;
-            case ExplodingKittens:
-                params = new ExplodingKittenParameters(seed);
-                forwardModel = new ExplodingKittensForwardModel();
-                gameState = new ExplodingKittensGameState(params, nPlayers);
-                break;
-            case LoveLetter:
-                params = new LoveLetterParameters(seed);
-                forwardModel = new LoveLetterForwardModel();
-                gameState = new LoveLetterGameState(params, nPlayers);
-                break;
-            case Uno:
-                params = new UnoGameParameters(seed);
-                forwardModel = new UnoForwardModel();
-                gameState = new UnoGameState(params, nPlayers);
-                break;
-            case Virus:
-                params = new VirusGameParameters(seed);
-                forwardModel = new VirusForwardModel();
-                gameState = new VirusGameState(params, nPlayers);
-                break;
-        }
-
-        if (forwardModel != null) {
-            return new Game(this, forwardModel, gameState);
-        }
-        return null;
-    }
-
-    /**
-     * Creates a graphical user interface for the given game type.
-     * @param gameState - initial game state from the game.
-     * @param ac - ActionController object allowing for user interaction with the GUI.
-     * @return - GUI for the given game type.
-     */
-    @SuppressWarnings("SwitchStatementWithTooFewBranches")
-    public AbstractGUI createGUI(AbstractGameState gameState, ActionController ac) {
-
-        AbstractGUI gui = null;
-
-        switch(this) {
-            case Pandemic:
-                gui = new PandemicGUI(gameState, ac);
-                break;
-        }
-
-        return gui;
     }
 }
