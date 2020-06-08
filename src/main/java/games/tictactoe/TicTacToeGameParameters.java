03e4eb1ddb2b08d90712ec9c14f22a6b73d86f56
Raluca D. Gaina <r.d.gaina@qmul.ac.uk>
2020-06-08 20:12:59 +0100

    Parameters can be tuned! (or just randomized for now)

--- src/main/java/games/tictactoe/TicTacToeGameParameters.java
+++ src/main/java/games/tictactoe/TicTacToeGameParameters.java
@@ -1,8 +1,13 @@
 package games.tictactoe;
 
 import core.AbstractGameParameters;
+import core.interfaces.TunableParameters;
 
-public class TicTacToeGameParameters extends AbstractGameParameters {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class TicTacToeGameParameters extends AbstractGameParameters implements TunableParameters {
     public int gridSize = 3;
 
     public TicTacToeGameParameters(long seed) {
@@ -15,4 +20,47 @@ public class TicTacToeGameParameters extends AbstractGameParameters {
         tttgp.gridSize = gridSize;
         return tttgp;
     }
+
+    @Override
+    public HashMap<Integer, ArrayList<?>> getSearchSpace() {
+        return new HashMap<Integer, ArrayList<?>>() {{
+            put(0, new ArrayList<Integer>() {{
+                add(3);
+                add(4);
+                add(5);
+            }});
+        }};
+    }
+
+    @Override
+    public List<Integer> getParameterIds() {
+        return new ArrayList<Integer>() {{
+            add(0);
+        }};
+    }
+
+    @Override
+    public Object getDefaultParameterValue(int parameterId) {
+        if (parameterId == 0) return 3;
+        return null;
+    }
+
+    @Override
+    public void setParameterValue(int parameterId, Object value) {
+        if (parameterId == 0) gridSize = (int) value;
+        else System.out.println("Unknown parameter " + parameterId);
+    }
+
+    @Override
+    public Object getParameterValue(int parameterId) {
+        if (parameterId == 0) return gridSize;
+        return null;
+    }
+
+    @Override
+    public String getParameterName(int parameterId) {
+        if (parameterId == 0) return "Grid size";
+        return null;
+    }
+
 }
