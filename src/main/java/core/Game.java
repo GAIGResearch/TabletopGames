ab952b852f08a9b355186342fc8d08cc7ad9bec0
diegopliebana <diego.perez@qmul.ac.uk>
2020-07-08 08:00:50 +0100

    Updating Game.java for an MCTS Player

--- src/main/java/core/Game.java
+++ src/main/java/core/Game.java
@@ -4,6 +4,7 @@ import core.actions.AbstractAction;
 import core.interfaces.IPrintable;
 import games.GameType;
 import players.*;
+import players.mcts.MCTSPlayer;
 import utilities.StatSummary;
 import utilities.Utils;
 
@@ -490,18 +491,25 @@ public class Game {
         players.add(new RandomPlayer(new Random()));
         players.add(new RandomPlayer(new Random()));
         players.add(new RandomPlayer(new Random()));
+//        players.add(new MCTSPlayer());
         players.add(new OSLA());
 //        players.add(new HumanGUIPlayer(ac));
 //        players.add(new HumanConsolePlayer());
 
         /* 4. Run! */
-//        runOne(TicTacToe, players, seed, ac, false);
+//        runOne(LoveLetter, players, seed, ac, false);
 //        runMany(GameType.Category.Strategy.getAllGames(), players, null, 50, null, false);
 
+//        ArrayList<GameType> games = new ArrayList<>();
+//        games.add(Pandemic);
+//        games.add(LoveLetter);
+
         ArrayList<GameType> games = new ArrayList<>(Arrays.asList(GameType.values()));
+        games.remove(LoveLetter);
         games.remove(Pandemic);
         games.remove(TicTacToe);
-        runMany(games, players, null, 50, null, false, false);
+//        runMany(games, players, null, 50, null, false, true);
+        runMany(games, players, null, 10, null, false, true);
 //        runMany(new ArrayList<GameType>() {{add(Uno);}}, players, null, 1000, null, false, false);
     }
 }
