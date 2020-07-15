d65e0ab4d357acdcee5210cead4ebecf3337b8ab
Raluca D. Gaina <r.d.gaina@qmul.ac.uk>
2020-07-15 14:46:35 +0100

    Update AIReport.java

--- src/main/java/evaluation/AIReport.java
+++ src/main/java/evaluation/AIReport.java
@@ -1,11 +1,20 @@
 package evaluation;
 
+import core.AbstractPlayer;
+import core.Game;
 import games.GameType;
+import players.RandomPlayer;
 import players.utils.PlayerType;
 
-import static games.GameType.Pandemic;
+import java.util.ArrayList;
+import java.util.List;
+
+import static games.GameType.*;
+import static players.utils.PlayerType.*;
 
 public class AIReport {
+
+    static int nRep = 50;
 
     /**
      * How fast is the player at making decisions.
@@ -13,7 +22,30 @@ public class AIReport {
      * @param game - game to test.
      */
     public static void actSpeed(PlayerType player, GameType game) {
+        System.out.println("--------------------\nAgent Speed Test: " + game.name() + " + " + player.name() + "\n--------------------");
 
+        int nPlayers = game.getMaxPlayers();
+        double nSpeed = 0;
+        for (int i = 0; i < nRep; i++) {
+            Game g = game.createGameInstance(nPlayers);
+            List<AbstractPlayer> players = new ArrayList<>();
+            for (int j = 0; j < nPlayers-1; j++) {
+                players.add(new RandomPlayer());
+            }
+            players.add(player.createPlayerInstance());
+
+            if (g != null) {
+                g.reset(players);
+                g.run();
+                nSpeed += g.getAgentTime();
+            }
+        }
+
+        if (nSpeed != 0) {
+            System.out.println("Player.getAction(): " + String.format("%6.3e", 1e+9 / (nSpeed / nRep)) + " executions/second");
+        }
+
+        System.out.println();
     }
 
     /**
@@ -28,6 +60,9 @@ public class AIReport {
     }
 
     public static void main(String[] args) {
-        run(PlayerType.Random, Pandemic);
+        for (PlayerType pt: Property.Human.getAllPlayersExcluding()) {
+            run(pt, LoveLetter);
+        }
+//        run(Random, Pandemic);
     }
 }
