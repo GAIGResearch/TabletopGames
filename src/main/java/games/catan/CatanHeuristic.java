d8426b7d66e6314ff39dc73ab903622ced383b38
Oliver Harrison <oharrison1998@outlook.com>
2021-03-16 14:39:38 +0000

    City and Settlement heuristic eval

--- src/main/java/games/catan/CatanHeuristic.java
+++ src/main/java/games/catan/CatanHeuristic.java
@@ -4,29 +4,31 @@ import core.AbstractGameState;
 import core.components.Card;
 import core.components.Deck;
 import core.interfaces.IStateHeuristic;
+import games.catan.components.Settlement;
 import utilities.Utils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static core.CoreConstants.playerHandHash;
 
 public class CatanHeuristic implements IStateHeuristic {
 
-    double FACTOR_PLAYER_SCORE = 0.7;
+    double FACTOR_PLAYER_SCORE = 0.5;
     double FACTOR_OPPONENTS_SCORE = -1.0/3.0;
-    double FACTOR_PLAYER_RESOURCES = 0.1;
-    double FACTOR_PLAYER_DEVELOPMENT_CARDS = 0.2;
+    double FACTOR_PLAYER_RESOURCES = 0.05;
+    double FACTOR_PLAYER_DEVELOPMENT_CARDS = 0.1;
+    double FACTOR_PLAYER_CITIES = 0.2;
+    double FACTOR_PLAYER_SETTLEMENTS = 0.15;
 
 
     @Override
     public double evaluateState(AbstractGameState gs, int playerId) {
         /**
          * TODO factors to evaluate:
-         * cities
-         * towns
          * ports
-         * knight count
-         * longest road closeness
+         * largest army
+         * longest road
          * development card advanced
          * opponents resource estimates
          */
@@ -55,6 +57,18 @@ public class CatanHeuristic implements IStateHeuristic {
         // player development card evaluation - very simple at the moment
         Deck<Card> playerDevDeck = (Deck<Card>)cgs.getComponentActingPlayer(CatanConstants.developmentDeckHash);
         stateValue += FACTOR_PLAYER_DEVELOPMENT_CARDS * (Math.min(playerDevDeck.getComponents().size(),10) * 0.1);
+        // player settlement and city evaluation
+        ArrayList<Settlement> settlements = cgs.getPlayersSettlements(playerId);
+        int settlementCount = 0, cityCount = 0;
+        for (int i = 0; i < settlements.size(); i++){
+            if (settlements.get(i).getType()==1){
+                settlementCount++;
+            } else {
+                cityCount++;
+            }
+        }
+        stateValue+= FACTOR_PLAYER_SETTLEMENTS * (settlementCount * 0.2);
+        stateValue+= FACTOR_PLAYER_CITIES * (cityCount * 0.25);
 
 
         return stateValue;
