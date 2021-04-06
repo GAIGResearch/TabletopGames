5a95283a4f2e0892b1f685614a705ac2becd4869
Oliver Harrison <oharrison1998@outlook.com>
2021-04-06 19:47:17 +0100

    Finished CRB design

--- src/main/java/players/simple/CatanRuleBasedPlayer.java
+++ src/main/java/players/simple/CatanRuleBasedPlayer.java
@@ -3,7 +3,10 @@ package players.simple;
 import core.AbstractGameState;
 import core.AbstractPlayer;
 import core.actions.AbstractAction;
+import core.actions.DoNothing;
 import games.catan.CatanGameState;
+import games.catan.actions.AcceptTrade;
+import games.catan.actions.OfferPlayerTrade;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,10 +18,7 @@ public class CatanRuleBasedPlayer extends AbstractPlayer {
     private final Random rnd; // random generator for selection of equally ranked actions
 
     // All current actions in the game, used for dynamically creating sub sets of the actions
-    //TODO : This can be done using isInstance for a more resilient system but that introduces the
-    // overheads of creating a new object to check against each time, not sure if it is worth it
     private enum ActionType {
-            DoNothing,
             AcceptTrade,
             BuildCity,
             BuildRoad,
@@ -44,44 +44,18 @@ public class CatanRuleBasedPlayer extends AbstractPlayer {
 
     public CatanRuleBasedPlayer() {this(new Random());}
 
-    /**
-     *
-     * @param gameState observation of the current game state
-     * @param possibleActions
-     * @return AbstractAction
-     */
     @Override
     public AbstractAction getAction(AbstractGameState gameState, List<AbstractAction> possibleActions) {
 
         List<List<AbstractAction>> actionLists = getActionSubsets((CatanGameState) gameState, possibleActions);
+        for(List<AbstractAction> actionList : actionLists){
+            if (actionList.size()>0){
+                return selectRandomAction(actionList);
+            }
+        }
 
-        // DEVELOPMENT CARDS
-        // Design a good scenario for when each development card should be played
-        // If the scenario is met, play the card
+        return null; // should never be reached
 
-        // TRADE
-        // first check if there are any trades available (need resources)
-        // identify which resource to request would be the best based on how close to building something
-        // randomly select a trade from this list
-
-        // BUY
-        // build city
-        // build settlement
-        // buy development card
-        // build road
-
-        // TRADE RESPONSE
-        // Identify if the offered resources is wanted
-        // If not, reject
-        // If yes but offer is bad, renegotiate
-        // If yes and offer is good, accept
-
-        // ROBBER
-        // Only place robber on tiles with settlements
-
-        // random select from all actions if all rules fail
-
-        return selectRandomAction(possibleActions);
     }
 
     public String toString() { return "CatanRuleBased";}
@@ -93,33 +67,79 @@ public class CatanRuleBasedPlayer extends AbstractPlayer {
 
     private List<List<AbstractAction>> getActionSubsets(CatanGameState cgs, List<AbstractAction> possibleActions){
         List<List<AbstractAction>> actionLists = new ArrayList<>();
-        actionLists.add(new ArrayList<>()); // build city actions
-        actionLists.add(new ArrayList<>()); // build settlement actions
-        actionLists.add(new ArrayList<>()); // buy development card actions
-        actionLists.add(new ArrayList<>()); // build road actions
+        actionLists.add(new ArrayList<>()); // highest priority actions - Play development cards
+        actionLists.add(new ArrayList<>()); // 2nd priority actions
+        actionLists.add(new ArrayList<>()); // 3rd priority actions
+        actionLists.add(new ArrayList<>()); // 4th priority actions
+        actionLists.add(new ArrayList<>()); // 5th priority actions
         actionLists.add(new ArrayList<>()); // default case actions
         ActionType actionType = null;
 
         for(AbstractAction action : possibleActions){
             actionType = ActionType.valueOf(action.getClass().getSimpleName());
             switch (actionType) {
-                case BuildCity:
-                    actionLists.get(0).add(action);
+                case PlayKnightCard:
+                    if(KnightCardCheck(cgs, action)){
+                        actionLists.get(0).add(action);
+                    }
                     break;
 
-                case BuildSettlement:
+                case Monopoly:
+                    if(MonopolyCardCheck(cgs, action)){
+                        actionLists.get(0).add(action);
+                    }
+                    break;
+
+                case YearOfPlenty:
+                    if(YearOfPlentyCardCheck(cgs, action)){
+                        actionLists.get(0).add(action);
+                    }
+                    break;
+
+                case PlaceRoad:
+                    //TODO currently bugged in game
+                    break;
+
+                case BuildCity:
                     actionLists.get(1).add(action);
                     break;
 
-                case BuyDevelopmentCard:
+                case BuildSettlement:
                     actionLists.get(2).add(action);
                     break;
 
+                case BuyDevelopmentCard:
+                    actionLists.get(3).add(action);
+                    break;
+
                 case BuildRoad:
-                    if(BuildRoadCheck(action)){
+                    if(BuildRoadCheck(cgs, action)){
+                        actionLists.get(4).add(action);
+                    }
+                    break;
+
+                case DefaultTrade:
+                    if(DefaultTradeCheck(cgs, action)){
+                        actionLists.get(2).add(action);
+                    }
+                    break;
+
+                case OfferPlayerTrade:
+                    if(OfferPlayerTradeCheck(cgs, action)){
                         actionLists.get(3).add(action);
                     }
                     break;
+
+                case MoveRobber:
+                    if(MoveRobberCheck(cgs,action)){
+                        actionLists.get(0).add(action);
+                    }
+
+                case AcceptTrade:
+                    if(AcceptTradeCheck(cgs,action)){
+                        actionLists.get(0).add(action);
+                    }
+
 
                 default:
                     actionLists.get(actionLists.size()-1).add(action);
@@ -129,8 +149,45 @@ public class CatanRuleBasedPlayer extends AbstractPlayer {
         return actionLists;
     }
 
-    private boolean BuildRoadCheck(AbstractAction action){
+    private boolean KnightCardCheck(CatanGameState cgs, AbstractAction action){
+        //TODO check if knight card should be played
+        return rnd.nextInt(2)==0;
+    }
+
+    private boolean MonopolyCardCheck(CatanGameState cgs, AbstractAction action){
+        //TODO check if monopoly card should be played
+        return rnd.nextInt(2)==0;
+    }
+
+    private boolean YearOfPlentyCardCheck(CatanGameState cgs, AbstractAction action){
+        //TODO check if Year of Plenty card should be played
+        return rnd.nextInt(2)==0;
+    }
+
+    private boolean BuildRoadCheck(CatanGameState cgs, AbstractAction action){
         //TODO workout a way of deciding whether or not building a road is a good idea?
+        return rnd.nextInt(2)==0;
+    }
+
+
+    private boolean DefaultTradeCheck(CatanGameState cgs, AbstractAction action){
+        //TODO check if default trade should be made
+        return rnd.nextInt(2)==0;
+    }
+
+    private boolean OfferPlayerTradeCheck(CatanGameState cgs, AbstractAction action){
+        //TODO check if trade should be offered
+        //TODO some state tracking for negotiations might be needed
+        return rnd.nextInt(2)==0;
+    }
+
+    private boolean MoveRobberCheck(CatanGameState cgs, AbstractAction action){
+        //TODO identify good spots to move the robber
+        return rnd.nextInt(2)==0;
+    }
+
+    private boolean AcceptTradeCheck(CatanGameState cgs, AbstractAction action){
+        //TODO identify good spots to move the robber
         return rnd.nextInt(2)==0;
     }
 }
