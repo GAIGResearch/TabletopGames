c5d7d98e0a394337616dfb4451278eb723287bbf
Oliver Harrison <oharrison1998@outlook.com>
2021-04-27 20:54:41 +0100

    Make Catan heuristic tunable

--- src/main/java/games/catan/CatanHeuristic.java
+++ src/main/java/games/catan/CatanHeuristic.java
@@ -1,86 +1,147 @@
 package games.catan;
 
 import core.AbstractGameState;
+import core.AbstractParameters;
 import core.components.Card;
 import core.components.Deck;
 import core.interfaces.IStateHeuristic;
+import evaluation.TunableParameters;
 import games.catan.components.Settlement;
 import utilities.Utils;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import static core.CoreConstants.playerHandHash;
+import static games.catan.CatanConstants.developmentDeckHash;
 
-public class CatanHeuristic implements IStateHeuristic {
+public class CatanHeuristic extends TunableParameters implements IStateHeuristic {
+    double playerScore = 0.4;
+    double playerResources = 0.05;
+    double playerDevelopmentCards = 0.05;
+    double playerCities = 0.25;
+    double playerSettlements = 0.15;
+    double playerPorts = 0.1;
+    double opponentsScore = -1;
 
-    double FACTOR_PLAYER_SCORE = 0.416;
-    double FACTOR_PLAYER_RESOURCES_COUNT = 0.0416;
-    double FACTOR_PLAYER_DEVELOPMENT_CARDS = 0.0416;
-    double FACTOR_PLAYER_CITIES = 0.2083;
-    double FACTOR_PLAYER_SETTLEMENTS = 0.1666;
-    double FACTOR_PLAYER_PORTS = 0.0833;
-    double FACTOR_PLAYER_KNIGHTS = 0.0416;
-    double FACTOR_OPPONENTS_SCORE = -1.0/3.0;
+    public CatanHeuristic() {
+        addTunableParameter("playerScore", 0.4);
+        addTunableParameter("playerResources", 0.05);
+        addTunableParameter("playerDevelopmentCards", 0.05);
+        addTunableParameter("playerCities", 0.25);
+        addTunableParameter("playerSettlements", 0.15);
+        addTunableParameter("playerPorts", 0.1);
+        addTunableParameter("opponentsScore", -1);
+    }
 
+    @Override
+    public void _reset() {
+        playerScore = (double) getParameterValue("playerScore");
+        playerResources = (double) getParameterValue("playerResources");
+        playerDevelopmentCards = (double) getParameterValue("playerDevelopmentCards");
+        playerCities = (double) getParameterValue("playerCities");
+        playerSettlements = (double) getParameterValue("playerSettlements");
+        playerPorts = (double) getParameterValue("playerPorts");
+        opponentsScore = (double) getParameterValue("opponentsScore");
+    }
 
     @Override
     public double evaluateState(AbstractGameState gs, int playerId) {
-        CatanGameState cgs = (CatanGameState)gs;
-        Utils.GameResult gameStatus = cgs.getGameStatus();
+        CatanGameState state = (CatanGameState) gs;
+        Utils.GameResult playerResult = state.getPlayerResults()[playerId];
 
-        if(gameStatus == Utils.GameResult.LOSE)
+        if (playerResult == Utils.GameResult.LOSE)
             return -1;
-        if(gameStatus == Utils.GameResult.WIN)
+        if (playerResult == Utils.GameResult.WIN)
             return 1;
 
-        double stateValue = 0;
-        // scores evaluation
-        int[] scores = cgs.getScores();
-        for (int i = 0; i < scores.length; i++){
-            if (i == playerId){
-                stateValue+=FACTOR_PLAYER_SCORE*(scores[i]*0.1);
+        double stateValue = 0.0;
+
+        // value the players and opponents scores by score and vic points for each player then dividing by total required to win
+        if(playerScore != 0.0 || opponentsScore != 0.0){
+            int[] scores = state.getScores();
+            int[] victoryPoints = state.getVictoryPoints();
+            for(int i = 0; i < scores.length; i++){
+                if (i != playerId){
+                    stateValue += opponentsScore * ((((scores[i] + victoryPoints[i]))
+                            / (double)((CatanParameters)state.getGameParameters()).points_to_win)/3.0);
+                } else {
+                    stateValue += playerScore * (((scores[i] + victoryPoints[i]))
+                            / (double)((CatanParameters)state.getGameParameters()).points_to_win);
+                }
             }
-            else {
-                stateValue+=FACTOR_OPPONENTS_SCORE*(scores[i]*0.1);
-            }
+
+            stateValue += playerScore * ((double) (state.getScores()[playerId] + state.getVictoryPoints()[playerId]))
+                    / ((CatanParameters)state.getGameParameters()).points_to_win;
         }
-        // player resource evaluation
-        List<Card> playerHand = ((Deck<Card>)cgs.getComponent(playerHandHash,playerId)).getComponents();
-        stateValue += FACTOR_PLAYER_RESOURCES_COUNT * (Math.min(playerHand.size(),7) * (1.0/7.0));
-        // player development card evaluation - very simple at the moment
-        Deck<Card> playerDevDeck = (Deck<Card>)cgs.getComponentActingPlayer(CatanConstants.developmentDeckHash);
-        stateValue += FACTOR_PLAYER_DEVELOPMENT_CARDS * (Math.min(playerDevDeck.getComponents().size(),10) * 0.1);
-        // player settlement and city evaluation
-        ArrayList<Settlement> settlements = cgs.getPlayersSettlements(playerId);
-        int settlementCount = 0, cityCount = 0;
-        for (int i = 0; i < settlements.size(); i++){
-            if (settlements.get(i).getType()==1){
-                settlementCount++;
-            } else {
-                cityCount++;
-            }
+
+        // value total resources, caps out at 7
+        if(playerResources != 0.0){
+            stateValue += playerResources * Math.min(((double) ((Deck<Card>)state.getComponent(playerHandHash,playerId)).getSize())/7.0,1.0);
         }
-        stateValue+= FACTOR_PLAYER_SETTLEMENTS * (settlementCount * 0.2);
-        stateValue+= FACTOR_PLAYER_CITIES * (cityCount * 0.25);
-        // Player port control evaluation
-        int[] playerExchangeRate = cgs.getExchangeRates(); // {4, 4, 4, 4, 4}
-        int portControlCount = 0;
-        for (int i = 0; i < playerExchangeRate.length; i++){
-            if (playerExchangeRate[i] < 4) {
-                portControlCount++;
-            }
+
+        // value development card count
+        if(playerDevelopmentCards != 0){
+            stateValue += playerDevelopmentCards * Math.min(((double) ((Deck<Card>)state.getComponent(developmentDeckHash,playerId)).getSize())/3.0,1.0);
         }
-        stateValue+= FACTOR_PLAYER_PORTS * (portControlCount * 0.2);
-        // Player knight count evaluation
-        int[] knights = cgs.getKnights();
-        int playerKnightsCount = knights[playerId];
-        int highestKnightsCount = Arrays.stream(knights).max().getAsInt();
-        if (highestKnightsCount != 0){
-            stateValue += FACTOR_PLAYER_KNIGHTS * (((double) ((100 / highestKnightsCount^2) * playerKnightsCount^2))/100);
+
+        // value player cities and settlements
+        if(playerCities != 0.0 || playerSettlements != 0.0){
+            int settlementCount = 0, cityCount = 0;
+            boolean[] harbours = new boolean[6]; // 0: brick, 1: lumber, 2: ore, 3: grain, 4: wool, 5: generic
+            ArrayList<Settlement> settlements = state.getPlayersSettlements(playerId);
+            for (Settlement settlement : settlements) {
+                if(settlement.getType()==1)
+                    settlementCount++;
+                else
+                    cityCount++;
+            }
+            stateValue += playerCities * (cityCount/4.0) + playerSettlements * (settlementCount/5.0);
+        }
+
+        // value player ports
+        if (playerPorts != 0.0){
+            int[] playerExchangeRates = state.getExchangeRates(playerId);
+            for(int exchangeRate : playerExchangeRates){
+                    if(exchangeRate < ((CatanParameters)state.getGameParameters()).default_exchange_rate - 1)
+                        stateValue += playerPorts * 0.2;
+                    else if (exchangeRate < ((CatanParameters)state.getGameParameters()).default_exchange_rate)
+                        stateValue += playerPorts * 0.1;
+            }
         }
 
         return stateValue;
+    }
+
+    @Override
+    protected AbstractParameters _copy() {
+        CatanHeuristic retValue = new CatanHeuristic();
+        retValue.playerScore = playerScore;
+        retValue.playerResources = playerResources;
+        retValue.playerDevelopmentCards = playerDevelopmentCards;
+        retValue.playerCities = playerCities;
+        retValue.playerSettlements = playerSettlements;
+        retValue.playerPorts = playerPorts;
+        retValue.opponentsScore = opponentsScore;
+        return retValue;
+    }
+
+    @Override
+    protected boolean _equals(Object o) {
+        if (o instanceof CatanHeuristic) {
+            CatanHeuristic other = (CatanHeuristic) o;
+            return other.playerScore == playerScore
+                    && other.playerResources == playerResources
+                    && other.playerDevelopmentCards == playerDevelopmentCards
+                    && other.playerCities == playerCities
+                    && other.playerSettlements == playerSettlements
+                    && other.playerPorts == playerPorts
+                    && other.opponentsScore == opponentsScore;
+        }
+        return false;
+    }
+
+    @Override
+    public Object instantiate() {
+        return _copy();
     }
 }
