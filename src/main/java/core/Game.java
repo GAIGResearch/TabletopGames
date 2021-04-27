80068f5b73b6604a8726d3b612ab8978aa61a7f6
Mohammed Shahidul Islam <pr3dator2193@gmail.com>
2021-04-27 22:44:36 +0100

    Added DLLs files for compatibility
    Needed if MingW compiler is not installed onto the machine

--- src/main/java/core/Game.java
+++ src/main/java/core/Game.java
@@ -690,8 +690,8 @@ public class Game {
      */
     public static void main(String[] args) {
         /* 1. Action controller for GUI interactions. If set to null, running without visuals. */
-        ActionController ac = new ActionController(); //null;
-        //ActionController ac = null;
+        //ActionController ac = new ActionController(); //null;
+        ActionController ac = null;
 
         /* 2. Game seed */
         long seed = System.currentTimeMillis(); //0;
@@ -704,10 +704,10 @@ public class Game {
         //players.add(new RandomPlayer());
 //        players.add(new RMHCPlayer());
 //        players.add(new MCTSPlayer(params1));
-        players.add(new HumanGUIPlayer(ac));
-        players.add(new HumanGUIPlayer(ac));
-        //players.add(new HumanConsolePlayer());
-        //players.add(new HumanConsolePlayer());
+        //players.add(new HumanGUIPlayer(ac));
+        //players.add(new HumanGUIPlayer(ac));
+        players.add(new HumanConsolePlayer());
+        players.add(new HumanConsolePlayer());
 
         /* 4. Run! */
         runOne(Poker, players, seed, ac, false, null);
