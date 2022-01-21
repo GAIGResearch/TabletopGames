166c82b029c539301b62d37e0ae1bc9f92658bc5
hopshackle <james@janigo.co.uk>
2022-01-21 17:35:47 +0000

    Added 'One Action' button to Frontend

--- src/main/java/gui/Frontend.java
+++ src/main/java/gui/Frontend.java
@@ -312,6 +312,19 @@ public class Frontend extends GUI {
         });
         gameControlButtons.add(resumeGame);
 
+        JButton oneAction = new JButton("Next Action");
+        oneAction.addActionListener(e -> {
+            if (gameRunning != null) {
+                // if the thread is running then we pause it first
+                gameRunning.setPaused(true);
+                // and then take a single action
+                gameRunning.oneAction();
+                // TODO: We also then want to display the actions available to the current player
+                // either a different mode, or standard?
+            }
+        });
+        gameControlButtons.add(oneAction);
+
         // todo tournaments, game report, player report etc
 
         // Put all together
