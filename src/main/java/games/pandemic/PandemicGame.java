f385e65f1013ef5ae32956d6031ab04d502919cc
martinballa <ballamartin95@gmail.com>
2022-03-11 15:09:56 +0000

    Added a new method to run the competition setup for Pandemic with some game over related logging. Added a super easy config as well

--- src/main/java/games/pandemic/PandemicGame.java
+++ src/main/java/games/pandemic/PandemicGame.java
@@ -1,12 +1,15 @@
 package games.pandemic;
 
 import core.*;
+import core.interfaces.IGameListener;
 import games.GameType;
 import games.pandemic.gui.PandemicGUIManager;
 import players.human.ActionController;
+import players.mcts.MCTSPlayer;
 import players.simple.OSLAPlayer;
 import players.simple.RandomPlayer;
-import utilities.Utils;
+import utilities.FileStatsLogger;
+import utilities.TAGStatSummary;
 
 import java.util.*;
 
@@ -20,45 +23,95 @@ public class PandemicGame extends Game {
         super(GameType.Pandemic, model, gameState);
     }
 
-    public static void main(String[] args){
+    public static Game runCompetition(GameType gameToPlay, String parameterConfigFile, List<AbstractPlayer> players, long seed,
+                          boolean randomizeParameters, List<IGameListener> listeners, int nRepetitions, ActionController ac){
+        int nPlayers = players.size();
+        boolean detailedStatistics = true;
 
-        ActionController ac = new ActionController();
+        // Save win rate statistics over all games
+        TAGStatSummary[] overall = new TAGStatSummary[nPlayers];
+        String[] agentNames = new String[nPlayers];
+        for (int i = 0; i < nPlayers; i++) {
+            String[] split = players.get(i).getClass().toString().split("\\.");
+            String agentName = split[split.length - 1] + "-" + i;
+            overall[i] = new TAGStatSummary("Overall " + agentName);
+            agentNames[i] = agentName;
+        }
 
-        List<AbstractPlayer> players = new ArrayList<>();
-        players.add(new RandomPlayer(new Random()));
-        players.add(new RandomPlayer(new Random()));
-        players.add(new RandomPlayer(new Random()));
-//        players.add(new HumanGUIPlayer(ac));
-        players.add(new OSLAPlayer());
+        // Save win rate statistics over all repetitions of this game
+        TAGStatSummary[] statSummaries = new TAGStatSummary[nPlayers];
+        for (int i = 0; i < nPlayers; i++) {
+            statSummaries[i] = new TAGStatSummary("{Game: " + gameToPlay.name() + "; Player: " + agentNames[i] + "}");
+        }
 
+        // Play n repetitions of this game and record player results
+        Game game = null;
+        int offset = 0;
+        for (int i = 0; i < nRepetitions; i++) {
+            Long s = seed;
+            if (s == null) s = System.currentTimeMillis();
+            s += offset;
+            game = runOne(gameToPlay, parameterConfigFile, players, s, randomizeParameters, listeners, ac, 0);
+            if (game != null) {
+                recordPlayerResults(statSummaries, game);
+                offset = game.getGameState().getTurnOrder().getRoundCounter() * game.getGameState().getNPlayers();
+            } else {
+                break;
+            }
+        }
 
-        PandemicParameters params = new PandemicParameters("data/pandemic/", System.currentTimeMillis());
-        PandemicGame game = new PandemicGame(players, params);
+        if (game != null) {
+            System.out.println("---------------------");
+            for (int i = 0; i < nPlayers; i++) {
+                // Print statistics for this game
+                if (detailedStatistics) {
+                    System.out.println(statSummaries[i].toString());
+                } else {
+                    System.out.println(statSummaries[i].name + ": " + statSummaries[i].mean() + " (n=" + statSummaries[i].n() + ")");
+                }
 
-//        AbstractGUI gui = new PandemicGUIManager(game, ac); TODO
-//        game.run(gui); TODO
+                // Record in overall statistics
+                overall[i].add(statSummaries[i]);
+            }
+        }
 
-//        game.run(null);
-        System.out.println(game.gameState.getGameStatus());
+        // Print final statistics
+        System.out.println("\n=====================\n");
+        for (int i = 0; i < nPlayers; i++) {
+            // Print statistics for this game
+            if (detailedStatistics) {
+                System.out.println(overall[i].toString());
+            } else {
+                System.out.println(overall[i].name + ": " + overall[i].mean());
+            }
+        }
 
-//        runMany(players, forwardModel);
+        return game;
     }
 
-    public static void runMany(List<AbstractPlayer> players, AbstractForwardModel model) {
-        HashMap<Utils.GameResult, Integer> results = new HashMap<>();
-        for (Utils.GameResult r: Utils.GameResult.values()) {
-            results.put(r, 0);
+    public static void main(String[] args){
+
+        String logFile = "results.csv";
+        ActionController ac = null; // new ActionController();
+        int nPlayers = 4;
+        int nRepetition = 10;
+
+        // logging setup
+        FileStatsLogger logger = new FileStatsLogger(logFile);
+        PandemicListener pl = new PandemicListener(logger);
+        ArrayList<IGameListener> listeners = new ArrayList<>();
+        listeners.add((pl));
+
+        List<AbstractPlayer> players = new ArrayList<>();
+
+        for (int i = 0; i < nPlayers; i++){
+//            players.add(new MCTSPlayer());
+            players.add(new RandomPlayer());
         }
 
         PandemicParameters params = new PandemicParameters("data/pandemic/", System.currentTimeMillis());
-        for (int i = 0; i < 10000; i++) {
-            PandemicGame game = new PandemicGame(players, params);
-            game.run();
-            Utils.GameResult result = game.gameState.getGameStatus();
-            int prevCount = results.get(result);
-            results.put(result, prevCount + 1);
-        }
+        Game game = runCompetition(GameType.Pandemic, "data/pandemic/param-config-easy.json", players, params.getRandomSeed(), false, listeners, nRepetition, ac);
+        logger.processDataAndFinish();
 
-        System.out.println(results.toString());
     }
 }
