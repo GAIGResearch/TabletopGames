1f06e791c0171bf0e25a65de74a115b37a8f88ce
hopshackle <james@janigo.co.uk>
2022-05-05 17:19:03 +0100

    Added LogisticStateHeuristic

---
+++ src/main/java/players/heuristics/AbstractStateHeuristic.java
@@ -0,0 +1,52 @@
+package players.heuristics;
+
+import core.interfaces.IStateFeatureVector;
+import core.interfaces.IStateHeuristic;
+
+import java.io.*;
+import java.util.Arrays;
+
+/**
+ * Provides a wrapper around an IStateFeatureVector and an array of coefficients
+ */
+public abstract class AbstractStateHeuristic implements IStateHeuristic {
+
+    protected IStateFeatureVector features;
+    protected double[] coefficients;
+
+    public AbstractStateHeuristic(String featureVectorClassName, String coefficientsFile) {
+        try {
+            features = (IStateFeatureVector) Class.forName(featureVectorClassName).getConstructor().newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new AssertionError("Problem with Class : " + featureVectorClassName);
+        }
+        loadModel(coefficientsFile);
+    }
+
+    public AbstractStateHeuristic(IStateFeatureVector featureVector, String coefficientsFile) {
+        this.features = featureVector;
+        loadModel(coefficientsFile);
+    }
+    private void loadModel(String coefficientsFile) {
+        if (coefficientsFile.isEmpty()) {
+            throw new AssertionError("No file for coefficients specified for AbstractStateHeuristic");
+        } else {
+            File coeffFile = new File(coefficientsFile);
+            try (BufferedReader br = new BufferedReader(new FileReader(coeffFile))) {
+                String[] headers = br.readLine().split("\\t");
+                if (!Arrays.equals(headers, features.names())) {
+                    throw new AssertionError("Incompatible data in file " + coeffFile);
+                }
+                coefficients = Arrays.stream(br.readLine().split("\\t")).mapToDouble(Double::parseDouble).toArray();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                throw new AssertionError("File not found : " + coeffFile);
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw new AssertionError("Error accessing : " + coeffFile);
+            }
+        }
+    }
+
+}
--- src/main/java/players/heuristics/LogisticStateHeuristic.java
+++ src/main/java/players/heuristics/LogisticStateHeuristic.java
@@ -4,12 +4,12 @@ import core.AbstractGameState;
 import core.interfaces.IStateFeatureVector;
 
 
-public class LinearStateHeuristic extends AbstractStateHeuristic {
+public class LogisticStateHeuristic extends AbstractStateHeuristic {
 
-    public LinearStateHeuristic(String featureVectorClassName, String coefficientsFile) {
+    public LogisticStateHeuristic(String featureVectorClassName, String coefficientsFile) {
         super(featureVectorClassName, coefficientsFile);
     }
-    public LinearStateHeuristic(IStateFeatureVector featureVector, String coefficientsFile) {
+    public LogisticStateHeuristic(IStateFeatureVector featureVector, String coefficientsFile) {
         super(featureVector, coefficientsFile);
     }
 
@@ -20,6 +20,6 @@ public class LinearStateHeuristic extends AbstractStateHeuristic {
         for (int i = 0; i < phi.length; i++) {
             retValue += phi[i] * coefficients[i];
         }
-        return retValue;
+        return 1.0 / ( 1.0 + Math.exp(-retValue));
     }
 }
