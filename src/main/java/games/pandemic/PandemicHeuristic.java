0416e8092bb85179db650f1b63ea24c779197ca4
Martin Balla <m.balla@qmul.ac.uk>
2022-05-06 16:34:42 +0100

    Added a new factor to the heuristic and tweaked some of the values

--- src/main/java/games/pandemic/PandemicHeuristic.java
+++ src/main/java/games/pandemic/PandemicHeuristic.java
@@ -2,34 +2,40 @@ package games.pandemic;
 
 import core.AbstractGameState;
 import core.CoreConstants;
+import core.components.Card;
 import core.components.Counter;
 import core.components.Deck;
 import core.interfaces.IStateHeuristic;
+import core.properties.PropertyColor;
 import core.properties.PropertyString;
 import evaluation.TunableParameters;
 import utilities.Hash;
 import utilities.Utils;
 
+import static core.CoreConstants.colorHash;
 import static games.pandemic.PandemicConstants.*;
 import static utilities.Utils.indexOf;
 
 public class PandemicHeuristic extends TunableParameters implements IStateHeuristic {
 
-    double FACTOR_CURES = 1.0;
-    double FACTOR_CUBES = 0.2;
-    double FACTOR_CARDS_IN_PILE = 0.15;
-    double FACTOR_CARDS_IN_HAND = 0.25;
-    double FACTOR_OUTBREAKS = -0.5;
-    double FACTOR_RS = 0.35;
-    double FACTOR_AT_RS = 0.6;
+    double FACTOR_CURES = 0.6;
+    double FACTOR_CUBES = 0.1;
+    double FACTOR_CARDS_IN_PILE = 0.1;
+    double FACTOR_CARDS_IN_HAND = 0.2;
+    double FACTOR_OUTBREAKS = -0.2;
+    double FACTOR_RS = 0.2; // Factor for having more RS
+    double FACTOR_AT_RS = 0.3; // Factor for current player standing at research station
+    double FACTOR_GOOD_CARDS = 0.3; // number of cards for uncured disease
+    // TODO: heuristic factors should be normalised
 
     public PandemicHeuristic() {
         addTunableParameter("FACTOR_CURES", 0.3);
-        addTunableParameter("FACTOR_CUBES", 0.2);
-        addTunableParameter("FACTOR_CARDS_IN_PILE", 0.15);
-        addTunableParameter("FACTOR_CARDS_IN_HAND", 0.15);
+        addTunableParameter("FACTOR_CUBES", 0.1);
+        addTunableParameter("FACTOR_CARDS_IN_PILE", 0.1);
+        addTunableParameter("FACTOR_CARDS_IN_HAND", 0.2);
         addTunableParameter("FACTOR_OUTBREAKS", -0.2);
         addTunableParameter("FACTOR_RS", 0.2);
+        addTunableParameter("FACTOR_GOOD_CARDS", 0.3);
     }
 
     @Override
@@ -40,6 +46,8 @@ public class PandemicHeuristic extends TunableParameters implements IStateHeuris
         FACTOR_CARDS_IN_HAND = (double) getParameterValue("FACTOR_CARDS_IN_HAND");
         FACTOR_OUTBREAKS = (double) getParameterValue("FACTOR_OUTBREAKS");
         FACTOR_RS = (double) getParameterValue("FACTOR_RS");
+        FACTOR_AT_RS = (double) getParameterValue("FACTOR_AT_RS");
+        FACTOR_GOOD_CARDS = (double) getParameterValue("FACTOR_GOOD_CARDS");
     }
 
     @Override
@@ -60,6 +68,13 @@ public class PandemicHeuristic extends TunableParameters implements IStateHeuris
         int nCardsInHand = ((Deck) pgs.getComponentActingPlayer(CoreConstants.playerHandHash)).getSize() / (pp.maxCardsPerPlayer + 2);
         int nResearchStations = ((Counter) pgs.getComponent(PandemicConstants.researchStationHash)).getValue() / pp.nResearchStations;
 
+        int[] colourCount = new int[colors.length]; // keeps track of the cards required to cure diseases
+        for( Card c : ((Deck<Card>) pgs.getComponentActingPlayer(CoreConstants.playerHandHash)).getComponents()) {
+            if (c.getProperty(colorHash) != null) {
+                colourCount[Utils.indexOf(colors, ((PropertyColor) c.getProperty(colorHash)).valueStr)]++;
+            }
+        }
+
         int playerAtResStation = 0;
         for (String resStationLocation: pgs.researchStationLocations){
             if (((PropertyString) pgs.getComponentActingPlayer(playerCardHash).getProperty(playerLocationHash)).value.equals(resStationLocation)){
@@ -72,9 +87,17 @@ public class PandemicHeuristic extends TunableParameters implements IStateHeuris
 
         for (int i = 0; i < colors.length; i++) {
             nDiseaseCubes += ((Counter) pgs.getComponent(Hash.GetInstance().hash("Disease Cube " + colors[i]))).getValue();
-            if (((Counter) pgs.getComponent(Hash.GetInstance().hash("Disease Cube " + colors[i]))).getValue() > 0)
+            if (((Counter) pgs.getComponent(Hash.GetInstance().hash("Disease " + colors[i]))).getValue() > 0) {
                 nCuresDiscovered += 1;
+                colourCount[i] = 0; // remove cards for cured diseases from the count
+            }
         }
+
+        double usefulCards = 0.0;
+        for (int i = 0; i < colourCount.length; i++){
+            usefulCards += colourCount[i];
+        }
+        usefulCards = usefulCards / pp.maxCardsPerPlayer; // normalisation
 
         return (nCuresDiscovered / colors.length) * FACTOR_CURES
                 + nCardsInHand * FACTOR_CARDS_IN_HAND
@@ -83,6 +106,7 @@ public class PandemicHeuristic extends TunableParameters implements IStateHeuris
                 + nOutbreaks * FACTOR_OUTBREAKS
                 + nResearchStations * FACTOR_RS
                 + playerAtResStation * FACTOR_AT_RS
+                + usefulCards * FACTOR_GOOD_CARDS
                 ;
     }
 
