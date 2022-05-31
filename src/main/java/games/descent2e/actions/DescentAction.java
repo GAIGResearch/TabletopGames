924d232fd57dea02a1a9d02e7f810cf79927aafd
Raluca D. Gaina <r.d.gaina@qmul.ac.uk>
2022-05-31 14:34:06 +0100

    Rest action (#195)

--- src/main/java/games/descent2e/actions/DescentAction.java
+++ src/main/java/games/descent2e/actions/DescentAction.java
@@ -5,6 +5,7 @@ import core.actions.AbstractAction;
 import games.descent2e.DescentGameState;
 
 import java.util.HashSet;
+import java.util.Objects;
 
 public abstract class DescentAction extends AbstractAction {
 
@@ -24,4 +25,17 @@ public abstract class DescentAction extends AbstractAction {
     }
     public abstract boolean execute(DescentGameState gs);
     public abstract DescentAction copy();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof DescentAction)) return false;
+        DescentAction that = (DescentAction) o;
+        return Objects.equals(triggerPoints, that.triggerPoints);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(triggerPoints);
+    }
 }
