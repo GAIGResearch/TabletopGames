3073aac177609bc6fda4443a2a6806c7487c1dc3
hopshackle <james@janigo.co.uk>
2022-12-31 17:31:29 +0000

    Updated Sirius State Features

--- src/main/java/games/sirius/SiriusStateFeatures.java
+++ src/main/java/games/sirius/SiriusStateFeatures.java
@@ -1,6 +1,7 @@
 package games.sirius;
 
 import core.AbstractGameState;
+import core.components.Deck;
 import players.heuristics.AbstractStateFeature;
 
 import java.util.*;
@@ -10,9 +11,11 @@ import static games.sirius.SiriusConstants.SiriusCardType.*;
 
 public class SiriusStateFeatures extends AbstractStateFeature {
 
-    String[] localNames = new String[]{"SALE_SCORE", "HAND_SIZE", "AMMONIA_TRACK", "CONTRABAND_TRACK", "AMMONIA_DECK", "CONTRABAND_DECK",
-            "MEDAL_COUNT", "MEDAL_SCORE", "AM_2", "AM_3", "AM_4", "AM_5", "AM_6", "CM_2", "CM_3", "CM_4", "CM_5", "CM_6",
-            "CARTEL_1", "CARTEL_2", "CARTEL_3", "FAVOUR_CARDS", "AMMONIA_TOT", "CONT_TOT", "GLOWING"};
+    String[] localNames = new String[]{"AM_TRACK", "CB_TRACK", "AM_DECK", "CB_DECK", "MEDAL_COUNT",
+            "AM_MEDAL_SCORE", "CB_MEDAL_SCORE", "AM_MEDALS", "CB_MEDALS", "MEDAL_2", "MEDAL_3", "MEDAL_4", "MEDAL_5", "MEDAL_6",
+            "TOT_HAND", "AM_HAND", "CB_HAND", "FV_HAND", "AM3_HAND", "AM2_HAND", "CB3_HAND", "GC_HAND",
+            "TOT_SOLD", "AM3_SOLD", "CB3_SOLD", "GC_SOLD",
+            "CARTEL_1", "CARTEL_2", "CARTEL_3"};
 
     @Override
     protected double maxScore() {
@@ -34,32 +37,40 @@ public class SiriusStateFeatures extends AbstractStateFeature {
         SiriusGameState state = (SiriusGameState) gs;
         double[] retValue = new double[localNames.length];
 
-        retValue[0] = state.playerAreas.get(playerID).soldCards.getSize();
-        retValue[1] = state.playerAreas.get(playerID).deck.getSize();
-        retValue[2] = state.ammoniaTrack;
-        retValue[3] = state.contrabandTrack;
-        retValue[4] = state.ammoniaDeck.getSize();
-        retValue[5] = state.contrabandDeck.getSize(); // TODO
+        retValue[0] = state.ammoniaTrack;
+        retValue[1] = state.contrabandTrack;
+        retValue[2] = state.ammoniaDeck.getSize();
+        retValue[3] = state.contrabandDeck.getSize();
+        retValue[4] = state.getMedalsTaken();
         Set<Medal> medals = new HashSet<>(state.playerAreas.get(playerID).medals);
-        retValue[6] = medals.size();
-        retValue[7] = medals.stream().mapToInt(m -> m.value).sum();
-        retValue[8] = medals.contains(new Medal(AMMONIA, 2)) ? 1 : 0;
-        retValue[9] = medals.contains(new Medal(AMMONIA, 3)) ? 1 : 0;
-        retValue[10] = medals.contains(new Medal(AMMONIA, 4)) ? 1 : 0;
-        retValue[11] = medals.contains(new Medal(AMMONIA, 5)) ? 1 : 0;
-        retValue[12] = medals.contains(new Medal(AMMONIA, 6)) ? 1 : 0;
-        retValue[13] = medals.contains(new Medal(CONTRABAND, 2)) ? 1 : 0;
-        retValue[14] = medals.contains(new Medal(CONTRABAND, 3)) ? 1 : 0;
-        retValue[15] = medals.contains(new Medal(CONTRABAND, 4)) ? 1 : 0;
-        retValue[16] = medals.contains(new Medal(CONTRABAND, 5)) ? 1 : 0;
-        retValue[17] = medals.contains(new Medal(CONTRABAND, 6)) ? 1 : 0;
-        retValue[18] = state.getMoon(1).getCartelOwner() == playerID ? 1 : 0;
-        retValue[19] = state.getMoon(2).getCartelOwner() == playerID ? 1 : 0;
-        retValue[20] = state.getMoon(3).getCartelOwner() == playerID ? 1 : 0;
-        retValue[21] = state.getPlayerHand(playerID).stream().filter(c -> c.cardType == FAVOUR).count();
-        retValue[22] = state.getPlayerHand(playerID).stream().filter((c -> c.cardType == AMMONIA)).mapToInt(c -> c.value).sum();
-        retValue[23] = state.getPlayerHand(playerID).stream().filter((c -> c.cardType == CONTRABAND)).mapToInt(c -> c.value).sum();
-        retValue[24] = state.getPlayerHand(playerID).stream().filter((c -> c.cardType == CONTRABAND && c.value == 0)).count();
+        retValue[5] = medals.stream().filter(m -> m.medalType == AMMONIA).mapToInt(m -> m.value).sum();
+        retValue[6] = medals.stream().filter(m -> m.medalType == CONTRABAND).mapToInt(m -> m.value).sum();
+        retValue[7] = medals.stream().filter(m -> m.medalType == AMMONIA).count();
+        retValue[8] = medals.stream().filter(m -> m.medalType == CONTRABAND).count();
+        retValue[9] = medals.stream().filter(m -> m.value == 2).count();
+        retValue[10] = medals.stream().filter(m -> m.value == 3).count();
+        retValue[11] = medals.stream().filter(m -> m.value == 4).count();
+        retValue[12] = medals.stream().filter(m -> m.value == 5).count();
+        retValue[13] = medals.stream().filter(m -> m.value == 6).count();
+        Deck<SiriusCard> hand = state.getPlayerHand(playerID);
+        retValue[14] = hand.getSize();
+        retValue[15] = hand.stream().filter(c -> c.cardType == AMMONIA).count();
+        retValue[16] = hand.stream().filter(c -> c.cardType == CONTRABAND).count();
+        retValue[17] = hand.stream().filter(c -> c.cardType == FAVOUR).count();
+        retValue[18] = hand.stream().filter(c -> c.cardType == AMMONIA && c.value == 3).count();
+        retValue[19] = hand.stream().filter(c -> c.cardType == AMMONIA && c.value == 2).count();
+        retValue[20] = hand.stream().filter(c -> c.cardType == CONTRABAND && c.value == 3).count();
+        retValue[21] = hand.stream().filter(c -> c.cardType == CONTRABAND && c.value == 0).count();
+
+        Deck<SiriusCard> soldCards = state.playerAreas.get(playerID).soldCards;
+        retValue[22] = soldCards.getSize();
+        retValue[23] = soldCards.stream().filter(c -> c.cardType == AMMONIA && c.value == 3).count();
+        retValue[24] = soldCards.stream().filter(c -> c.cardType == CONTRABAND && c.value == 3).count();
+        retValue[25] = soldCards.stream().filter(c -> c.cardType == CONTRABAND && c.value == 0).count();
+
+        retValue[26] = state.getMoon(1).getCartelOwner() == playerID ? 1 : 0;
+        retValue[27] = state.getMoon(2).getCartelOwner() == playerID ? 1 : 0;
+        retValue[28] = state.getMoon(3).getCartelOwner() == playerID ? 1 : 0;
 
         return retValue;
     }
