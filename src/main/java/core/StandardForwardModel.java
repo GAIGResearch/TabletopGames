11af715391ee670637fda944e3258ed241bd3e66
hopshackle <james@janigo.co.uk>
2023-01-19 20:58:02 +0000

    restored _afterActionin StandardFM (DM tests now green)

--- src/main/java/core/StandardForwardModel.java
+++ src/main/java/core/StandardForwardModel.java
@@ -1,20 +1,17 @@
 package core;
 
 import core.actions.AbstractAction;
-import utilities.Utils;
 
 public abstract class StandardForwardModel extends AbstractForwardModel {
 
     @Override
-    protected void _next(AbstractGameState currentState, AbstractAction action){
+    protected void _next(AbstractGameState currentState, AbstractAction action) {
         if (action != null) {
             action.execute(currentState);
         } else {
             throw new AssertionError("No action selected by current player");
         }
-        if (! currentState.isActionInProgress()) {
-            _afterAction(currentState, action);
-        }
+        _afterAction(currentState, action);
     }
 
     protected abstract void _afterAction(AbstractGameState currentState, AbstractAction actionTaken);
