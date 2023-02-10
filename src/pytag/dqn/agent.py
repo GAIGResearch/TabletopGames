VArious updates - action masking is now used for DQN - fixed a bug with Diamant's observation not showing the gems on the path - WIP: adding PPO

src/pytag/dqn/agent.py
@@ -74,7 +74,7 @@ class DQNAgent():
         self.gamma = 0.95
         self.norm_clip = 3
         self.update_counter = 0
-        self.lr = 1e-1
+        self.lr = 1e-3
 
         self.mem = ReplayMemory(self.input_dims, batch_size=self.batch_size)
 
@@ -94,13 +94,18 @@ class DQNAgent():
 
 
 
-    def act(self, state, epsilon=0.0):
-        q = None
+    def act(self, state, mask, epsilon=0.0):
+        mask = torch.tensor(mask, dtype=torch.float32).to(self.device)
         if self.random.uniform(0, 1) < epsilon:
-            action = self.random.randint(0, self.n_actions)
+            available_a_idx, = torch.where(mask == 1.0)
+            action = np.random.choice(available_a_idx)
+            # action = self.random.randint(0, self.n_actions)
+            q = torch.zeros(self.n_actions, dtype=torch.float32).to(self.device)
+            q[action] = 1.0
         else:
             with torch.no_grad():
                 q = self.q_net(state)
+                q = q * mask
                 action = q.argmax(1).item()
                 q.detach().cpu().numpy()
         return action, q

src/pytag/main_ppo.py
@@ -7,18 +7,8 @@ import wandb
 import numpy as np
 
 from pyTAG import PyTAG
-from dqn.agent import DQNAgent
+from ppo.agent import PPO
 
-def get_epsilon(steps, max_steps, final_epsilon=0.0):
-  # linearly decay towards 0
-  lr = ((max_steps - steps) / max_steps)  # * 1 - final_epsilon
-  if (lr < final_epsilon):
-    lr = final_epsilon
-  return lr
-
-def mask_logits(logits, mask):
-    logits = logits * torch.tensor(mask, dtype=torch.float32)
-    return logits
 
 def process_obs(obs, device="cpu"):
     x = torch.from_numpy(obs)
@@ -26,33 +16,34 @@ def process_obs(obs, device="cpu"):
 
     return x
 
+def mask_logits(logits, mask):
+    logits * torch.tensor(mask)
+    return logits
+
 if __name__ == "__main__":
     max_reward = 1.0 # reward clipping
     disable_wandb = False
-    project_name = "TAG-DQN"
-    epsilon = 1.0
+    project_name = "TAG-PPO"
     if disable_wandb:
         wandb.init(project=project_name, mode="disabled")
     else:
         wandb.init(project=project_name)
-    MAX_STEPS = 1000000
-    replay_freq = 4 # number of steps
+    MAX_STEPS = 500000
+    replay_freq = 4000 # number of steps
     agents = ["python", "random"]
     env = PyTAG(agents=agents, game="Diamant")
-    agent = DQNAgent(env)
+    agent = PPO(env)
 
     start_time = time.time()
     wins = 0
     episodes = 0
     done = True
-    invalid_action = False
     ep_steps = 0
     running_wins = deque(maxlen=20)
     for step in range(MAX_STEPS):
         if done:
             # logging
             episodes += 1
-            epsilon = get_epsilon(step, MAX_STEPS, 0.05)
 
             if step > 0:
                 running_wins.append(env.has_won())
@@ -75,11 +66,10 @@ if __name__ == "__main__":
             agent.learn(step)
 
         ep_steps += 1
-        # masking happens on the agent side
-        action, q = agent.act(obs, env.getActionMask(), epsilon=epsilon)
+        action, log_probs = agent.act(obs)
 
         playerID = env.getPlayerID()
-        next_obs, reward, done, info = env.step(action) # todo reward should be the delta, not total
+        next_obs, reward, done, info = env.step(action)
         # reward = np.clip(reward, -1, max_reward) # value, min, max
 
         next_obs = process_obs(next_obs)