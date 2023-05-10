f7fcd1dd385f9d9df6666a96daf3d80d97baa6f6
Raluca D. Gaina <r.d.gaina@qmul.ac.uk>
2023-05-10 14:22:45 +0100

    Dominion metrics update

--- src/main/java/games/dominion/stats/DominionMetrics.java
+++ src/main/java/games/dominion/stats/DominionMetrics.java
@@ -1,45 +1,59 @@
-//package games.dominion.stats;
-//import evaluation.listeners.MetricsGameListener;
-//import evaluation.metrics.*;
-//import games.dominion.DominionConstants;
-//import games.dominion.DominionGameState;
-//import games.dominion.cards.CardType;
-//import utilities.Group;
-//
-//import java.util.Arrays;
-//import java.util.Collections;
-//import java.util.List;
-//import java.util.Set;
-//
-//@SuppressWarnings("unused")
-//public class DominionMetrics implements IMetricsCollection {
-//
-//    public static class VictoryCardsLeft extends AbstractParameterizedMetric {
-//        public VictoryCardsLeft(){super();}
-//        public VictoryCardsLeft(Object arg){super(arg);}
-//        @Override
-//        public Object run(MetricsGameListener listener, Event e) {
-//            CardType type = (CardType) getParameterValue("type");
-//            return ((DominionGameState)e.state).cardsOfType(type, -1, DominionConstants.DeckType.SUPPLY);
-//        }
-//        @Override
-//        public Set<Event.GameEvent> getDefaultEventTypes() {
-//            return Collections.singleton(Event.GameEvent.GAME_OVER);
-//        }
-//        public List<Group<String, List<?>, ?>> getAllowedParameters() {
-//            return Collections.singletonList(new Group<>("type", Arrays.asList(CardType.values()), CardType.LIBRARY));
-//        }
-//    }
-//
-//    public static class EmptySupplySlots extends AbstractMetric {
-//        @Override
-//        public Object run(MetricsGameListener listener, Event e) {
-//            return ((DominionGameState)e.state).cardsIncludedInGame().stream()
-//                    .filter(c -> ((DominionGameState)e.state).cardsOfType(c, -1, DominionConstants.DeckType.SUPPLY) == 0)
-//                    .count();
-//        }
-//        public Set<Event.GameEvent> getDefaultEventTypes() {
-//            return Collections.singleton(Event.GameEvent.GAME_OVER);
-//        }
-//    }
-//}
+package games.dominion.stats;
+
+import core.Game;
+import evaluation.listeners.MetricsGameListener;
+import evaluation.metrics.AbstractMetric;
+import evaluation.metrics.Event;
+import evaluation.metrics.IMetricsCollection;
+import games.dominion.DominionConstants;
+import games.dominion.DominionGameState;
+import games.dominion.cards.CardType;
+
+import java.util.*;
+
+@SuppressWarnings("unused")
+public class DominionMetrics implements IMetricsCollection {
+
+    public static class VictoryCardsLeft extends AbstractMetric {
+        @Override
+        public boolean _run(MetricsGameListener listener, Event e, Map<String, Object> records) {
+            for (CardType type : CardType.values()) {
+                records.put(type.toString(), ((DominionGameState)e.state).cardsOfType(type, -1, DominionConstants.DeckType.SUPPLY));
+            }
+            return true;
+        }
+
+        @Override
+        public Set<Event.GameEvent> getDefaultEventTypes() {
+            return Collections.singleton(Event.GameEvent.GAME_OVER);
+        }
+
+        @Override
+        public Map<String, Class<?>> getColumns(Game game) {
+            Map<String, Class<?>> columns = new HashMap<>();
+            for (CardType type : CardType.values()) {
+                columns.put(type.toString(), Integer.class);
+            }
+            return columns;
+        }
+    }
+
+    public static class EmptySupplySlots extends AbstractMetric {
+        @Override
+        public boolean _run(MetricsGameListener listener, Event e, Map<String, Object> records) {
+            records.put("EmptySupplySlots", (int)((DominionGameState)e.state).cardsIncludedInGame().stream()
+                    .filter(c -> ((DominionGameState)e.state).cardsOfType(c, -1, DominionConstants.DeckType.SUPPLY) == 0)
+                    .count());
+            return true;
+        }
+
+        public Set<Event.GameEvent> getDefaultEventTypes() {
+            return Collections.singleton(Event.GameEvent.GAME_OVER);
+        }
+
+        @Override
+        public Map<String, Class<?>> getColumns(Game game) {
+            return Collections.singletonMap("EmptySupplySlots", Integer.class);
+        }
+    }
+}
