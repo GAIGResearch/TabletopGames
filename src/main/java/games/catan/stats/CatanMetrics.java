ac3955c1359abab77ce206989f72df759fbfe1cc
Raluca D. Gaina <r.d.gaina@qmul.ac.uk>
2023-05-19 17:08:02 +0100

    Catan metrics plan

---
+++ src/main/java/games/catan/stats/CatanMetrics.java
@@ -0,0 +1,29 @@
+package games.catan.stats;
+
+import evaluation.metrics.IMetricsCollection;
+
+public class CatanMetrics implements IMetricsCollection {
+    public enum CatanEvent {
+
+    }
+
+    // Initial sum of production values
+    // Variety of starting resources (how many different types of resources)
+    // Starting resources (how many of each type)
+
+    // Settlements played on ports
+    // Settlements played adjacent to existing road
+
+    // Percentage of turns in the lead
+
+    // # Development cards bought
+    // # roads built
+    // # settlements built
+    // # cities built
+    // # knight cards played
+    // longest road length
+
+    // # times 7-outed
+
+    // # times longest road and largest army were taken by other players after initially claimed
+}
