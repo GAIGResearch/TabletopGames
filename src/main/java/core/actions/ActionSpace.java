b519eb40b68d206e8b70eff89cf0bdde536859b6
Raluca D. Gaina <r.d.gaina@qmul.ac.uk>
2023-06-23 10:54:10 +0100

    More documentation on action space properties

--- src/main/java/core/actions/ActionSpace.java
+++ src/main/java/core/actions/ActionSpace.java
@@ -5,8 +5,13 @@ import core.AbstractGameState;
 import java.util.Objects;
 
 /**
- * See {@link games.loveletter.LoveLetterForwardModel#_computeAvailableActions(AbstractGameState, ActionSpace)}
- * for example implementations of structured action spaces.
+ * <p>See {@link games.loveletter.LoveLetterForwardModel#_computeAvailableActions(AbstractGameState, ActionSpace)}
+ * for example implementations of structured action spaces.</p>
+ *
+ * <p>These are used within the forward model to compute the available actions for a given game state.
+ * Each call to this function can request different lists of actions for different types of action spaces.
+ * Ideally, the same action space should be kept throughout a game (otherwise deep action spaces might not receive the
+ * correct sequence of decisions required).</p>
  */
 public class ActionSpace {
     public final Structure structure;
@@ -44,16 +49,43 @@ public class ActionSpace {
         this.context = context;
     }
 
+    /**
+     * Defines the structure of the action space. Current options supported in some games:
+     * <ul>
+     *     <li>Default: whichever option the game considers by default, usually 'Flat'. Used when no other structure specified.</li>
+     *     <li>Flat: combinatorial list of full actions, considering all combinations of parameter values.</li>
+     *     <li>Deep: Actions with multiple parameters/decisions required are split into several decisions.</li>
+     * </ul>
+     */
     public enum Structure {
         Default,
         Flat,
         Deep
     }
+
+    /**
+     * Defines the flexibility of the action space. The options are currently NOT supported in any of the games.
+     * <ul>
+     *     <li>Default: whichever option the game considers by default, usually 'Rigid'. Used when no other flexibility specified.</li>
+     *     <li>Rigid: if multiple decisions need to be taken for an action, the order for these decisions is fixed.</li>
+     *     <li>Elastic: if multiple decisions need to be taken for an action, the agent has an additional choice to make, for
+     *     the order in which the decisions will be taken. Useful for e.g. decision trees to maximise use of information gain.</li>
+     * </ul>
+     */
     public enum Flexibility {
         Default,
         Rigid,
         Elastic
     }
+
+    /**
+     * Defines the context of the action space. Current options supported in some games:
+     * <ul>
+     *     <li>Default: whichever option the game considers by default, usually 'Independent'. Used when no other context specified.</li>
+     *     <li>Independent: the actions are self-contained and have the same effect in any game state they are applied to (e.g. play card 'King').</li>
+     *     <li>Dependent: actions require context from the game state, and effect will differ (e.g. play 3rd card in hand).</li>
+     * </ul>
+     */
     public enum Context {
         Default,
         Independent,
