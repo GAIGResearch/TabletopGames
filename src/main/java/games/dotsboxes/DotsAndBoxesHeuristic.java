69f22845a8fc40f225036879b594881e8acaadce
hopshackle <james@janigo.co.uk>
2023-06-28 10:42:35 +0100

    CoarseTunableHeuristic to allow tuning of Win/Leader/Score etc.

--- src/main/java/games/dotsboxes/DotsAndBoxesHeuristic.java
+++ src/main/java/games/dotsboxes/DotsAndBoxesHeuristic.java
@@ -76,13 +76,8 @@ public class DotsAndBoxesHeuristic extends TunableParameters implements IStateHe
      */
     @Override
     protected DotsAndBoxesHeuristic _copy() {
-        DotsAndBoxesHeuristic retValue = new DotsAndBoxesHeuristic();
-        retValue.POINTS = POINTS;
-        retValue.POINT_ADVANTAGE = POINT_ADVANTAGE;
-        retValue.THREE_BOXES = THREE_BOXES;
-        retValue.TWO_BOXES = TWO_BOXES;
-        retValue.ORDINAL = ORDINAL;
-        return retValue;
+        // all the parameters are then copied in ITunableParameters
+        return new DotsAndBoxesHeuristic();
     }
 
     /**
