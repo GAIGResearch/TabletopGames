2f167429d102b9d0450fd943d07b5de7042e6476
hopshackle <james@janigo.co.uk>
2023-09-10 11:34:25 +0100

    Merge ECS7002P preparation

--- src/main/java/players/basicMCTS/BasicMCTSParams.java
+++ src/main/java/players/basicMCTS/BasicMCTSParams.java
@@ -11,7 +11,7 @@ public class BasicMCTSParams extends PlayerParameters {
 
     public double K = Math.sqrt(2);
     public int rolloutLength = 10; // assuming we have a good heuristic
-    public int maxTreeDepth = 1000; // effectively no limit
+    public int maxTreeDepth = 100; // effectively no limit
     public double epsilon = 1e-6;
     public IStateHeuristic heuristic = AbstractGameState::getHeuristicScore;
 
@@ -23,7 +23,7 @@ public class BasicMCTSParams extends PlayerParameters {
         super(seed);
         addTunableParameter("K", Math.sqrt(2), Arrays.asList(0.0, 0.1, 1.0, Math.sqrt(2), 3.0, 10.0));
         addTunableParameter("rolloutLength", 10, Arrays.asList(0, 3, 10, 30, 100));
-        addTunableParameter("maxTreeDepth", 10, Arrays.asList(1, 3, 10, 30, 100));
+        addTunableParameter("maxTreeDepth", 100, Arrays.asList(1, 3, 10, 30, 100));
         addTunableParameter("epsilon", 1e-6);
         addTunableParameter("heuristic", (IStateHeuristic) AbstractGameState::getHeuristicScore);
     }
