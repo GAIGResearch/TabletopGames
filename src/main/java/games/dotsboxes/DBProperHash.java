47d0e8e415155b7bf6efe54b550f61a3443f1c1e
hopshackle <james@janigo.co.uk>
2023-09-19 19:14:58 +0100

    Fixes for DotsAndBoxes hash code in MCGS

--- src/main/java/games/dotsboxes/DBProperHash.java
+++ src/main/java/games/dotsboxes/DBProperHash.java
@@ -15,7 +15,7 @@ public class DBProperHash implements IStateFeatureVector {
         // nothing else matters (anyway)
         double[] retValue = new double[2];
         double[] edgesByMidPosition = dbgs.edgeToOwnerMap.keySet().stream().map(e -> 100 * (e.from.getX() + e.to.getX() / 2.0) +
-                (e.from.getY() + e.to.getY() / 2.0)).mapToDouble(i -> i).toArray();
+                (e.from.getY() + e.to.getY() / 2.0)).mapToDouble(i -> i).sorted().toArray();
         retValue[0] = Arrays.hashCode(edgesByMidPosition) % 10000000;
         retValue[1] = Arrays.hashCode(dbgs.nCellsPerPlayer);
         return retValue;
