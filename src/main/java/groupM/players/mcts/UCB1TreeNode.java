9e271eb96ad0c418e9411548d3de7e42f33e80a1
Malo Hamon <hamon.malo@gmail.com>
2023-10-15 12:40:06 +0100

    Extract the loop of nodes to abstract class

--- src/main/java/groupM/players/mcts/UCB1TreeNode.java
+++ src/main/java/groupM/players/mcts/UCB1TreeNode.java
@@ -4,7 +4,6 @@ import java.util.Random;
 import static utilities.Utils.noise;
 
 import core.AbstractGameState;
-import core.actions.AbstractAction;
 
 public class UCB1TreeNode extends TreeNode{
     
@@ -18,55 +17,6 @@ public class UCB1TreeNode extends TreeNode{
         totValue = 0.0;
     }
 
-
-    @Override
-    AbstractAction selectAction() {
-     // Find child with highest UCB value, maximising for ourselves and minimizing for opponent
-     AbstractAction bestAction = null;
-     double bestValue = -Double.MAX_VALUE;
-
-     for (AbstractAction action : children.keySet()) {
-        UCB1TreeNode child = (UCB1TreeNode) children.get(action);
-         if (child == null)
-             throw new AssertionError("Should not be here");
-         else if (bestAction == null)
-             bestAction = action;
-
-         // Find child value
-         double hvVal = child.totValue;
-         double childValue = hvVal / (child.nVisits + player.params.epsilon);
-         double explorationTerm = player.params.K * Math.sqrt(Math.log(this.nVisits + 1) / (child.nVisits + player.params.epsilon));
-
-         // Find 'UCB' value
-         // If 'we' are taking a turn we use classic UCB
-         // If it is an opponent's turn, then we assume they are trying to minimise our score (with exploration)
-         boolean iAmMoving = state.getCurrentPlayer() == player.getPlayerID();
-         double uctValue = iAmMoving ? childValue : -childValue;
-         uctValue += explorationTerm;
-
-         // Apply small noise to break ties randomly
-         uctValue = noise(uctValue, player.params.epsilon, player.rnd.nextDouble());
-
-         // Assign value
-         if (uctValue > bestValue) {
-             bestAction = action;
-             bestValue = uctValue;
-         }
-     }
-
-     if (bestAction == null)
-         throw new AssertionError("We have a null value in UCT : shouldn't really happen!");
-
-     root.fmCallsCount++;  // log one iteration complete
-     return bestAction;
-    }
-    
-    /**
-     * Back up the value of the child through all parents. Increase number of visits and total value.
-     *
-     * @param result - value of rollout to backup
-     */
-    @Override
     void backUp(double result) {
 
         UCB1TreeNode n = this;
@@ -76,40 +26,46 @@ public class UCB1TreeNode extends TreeNode{
             n = (UCB1TreeNode) n.parent;
         }
     }
-    
 
     @Override
-    /**
-     * Calculates the best action from the root according to the most visited node
-     *
-     * @return - the best AbstractAction
-     */
-    AbstractAction bestAction() {
-
-        double bestValue = -Double.MAX_VALUE;
-        AbstractAction bestAction = null;
-
-        for (AbstractAction action : children.keySet()) {
-            if (children.get(action) != null) {
-                UCB1TreeNode node = (UCB1TreeNode) children.get(action);
-                double childValue = node.nVisits;
-
-                // Apply small noise to break ties randomly
-                childValue = noise(childValue, player.params.epsilon, player.rnd.nextDouble());
-
-                // Save best value (highest visit count)
-                if (childValue > bestValue) {
-                    bestValue = childValue;
-                    bestAction = action;
-                }
-            }
-        }
-
-        if (bestAction == null) {
-            throw new AssertionError("Unexpected - no selection made.");
-        }
-
-        return bestAction;
+    double getChildValue(TreeNode child, boolean isExpanding) {
+         return isExpanding ? ucb1(child) : nVisitsWithNoise(child);
     }
+
+
+    private double ucb1(TreeNode child) {
+        UCB1TreeNode ucbChild = (UCB1TreeNode) child;
+
+        double hvVal = ucbChild.totValue;
+        double childValue = hvVal / (ucbChild.nVisits + player.params.epsilon);
+        double explorationTerm = player.params.K * Math.sqrt(Math.log(this.nVisits + 1) / (ucbChild.nVisits + player.params.epsilon));
+
+        // Find 'UCB' value
+        // If 'we' are taking a turn we use classic UCB
+        // If it is an opponent's turn, then we assume they are trying to minimise our score (with exploration)
+        boolean iAmMoving = state.getCurrentPlayer() == player.getPlayerID();
+        double uctValue = iAmMoving ? childValue : -childValue;
+        uctValue += explorationTerm;
+
+        // Apply small noise to break ties randomly
+        uctValue = noise(uctValue, player.params.epsilon, player.rnd.nextDouble());
+
+        return uctValue;
+    }
+    
+    private double nVisitsWithNoise(TreeNode child) {
+        UCB1TreeNode ucbChild = (UCB1TreeNode) child;
+
+        double childValue = ucbChild.nVisits;
+
+        // Apply small noise to break ties randomly
+        childValue = noise(childValue, player.params.epsilon, player.rnd.nextDouble());
+        return childValue;
+    }
+
+
+
+
+
 
 }
