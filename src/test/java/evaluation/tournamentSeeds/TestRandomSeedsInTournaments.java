f1a740bb3c4eb15eb4461b1f691c6d0546666f1e
hopshackle <james@janigo.co.uk>
2023-12-04 17:08:23 +0000

    Tests Green

--- src/test/java/evaluation/tournamentSeeds/TestRandomSeedsInTournaments.java
+++ src/test/java/evaluation/tournamentSeeds/TestRandomSeedsInTournaments.java
@@ -17,12 +17,10 @@ public class TestRandomSeedsInTournaments {
         // 10 games in each, with 10 different seeds (seeds the same across each pair of agents)
         RunGames.main(new String[]{"config=Tournament_Focus_Base.json"});
         // 30 games, with 10 different seeds...in each set the Focus player is p1, p1, p2 respectively
-        // TODO: This just uses 30 different seeds, not 10
 
         // the ones below should then replicate all of the above, but with 3 seeds
         RunGames.main(new String[]{"config=Tournament_Random_ThreeSeeds.json"});
         // 30 games, each set of 10 with a different seed
-        // TODO: This just uses 30 different seeds
         RunGames.main(new String[]{"config=Tournament_Exhaustive_ThreeSeeds.json"});
         // 3  * 60 games, with each set of 60 having the same seed
         RunGames.main(new String[]{"config=Tournament_Seq_ThreeSeeds.json"});
@@ -30,7 +28,6 @@ public class TestRandomSeedsInTournaments {
         // 3 * 10 games in each, with 3 different seeds (seeds the same across each pair of agents)
         RunGames.main(new String[]{"config=Tournament_Focus_ThreeSeeds.json"});
         // 3 * 30 games, with 1 seed per set of 30...in each set the Focus player is p1, p1, p2 respectively
-        // todo: This just uses 90 different seeds
     }
 
 }
