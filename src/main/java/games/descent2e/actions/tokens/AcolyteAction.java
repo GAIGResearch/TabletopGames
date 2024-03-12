20cf869b733fbcf57d0556177837d33c43659c2c
Toby-Best <t.j.best@qmul.ac.uk>
2024-03-12 14:58:25 +0000

    AcolyteAction now activates on end of turn, can choose only one Clergy to rescue

--- src/main/java/games/descent2e/actions/tokens/AcolyteAction.java
+++ src/main/java/games/descent2e/actions/tokens/AcolyteAction.java
@@ -1,7 +1,10 @@
 package games.descent2e.actions.tokens;
 
 import core.AbstractGameState;
+import core.actions.AbstractAction;
+import core.actions.DoNothing;
 import core.components.GridBoard;
+import core.interfaces.IExtendedSequence;
 import games.descent2e.DescentGameState;
 import games.descent2e.DescentTypes;
 import games.descent2e.components.DescentGridBoard;
@@ -9,8 +12,12 @@ import games.descent2e.components.Hero;
 import games.descent2e.components.tokens.DToken;
 import utilities.Vector2D;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 
+import static games.descent2e.actions.Triggers.ACTION_POINT_SPEND;
 import static games.descent2e.actions.Triggers.END_TURN;
 import static utilities.Utils.getNeighbourhood;
 
@@ -20,30 +27,108 @@ import static utilities.Utils.getNeighbourhood;
  * TODO: effect does not cost action points and is automatically triggered at the end of the hero's turn
  * TODO: player choice which one to get if multiple options (currently first token found)
  */
-public class AcolyteAction extends TokenAction {
+public class AcolyteAction extends TokenAction implements IExtendedSequence {
+
+    int villagerID;
+    boolean complete;
 
     public AcolyteAction() {
         super(-1, END_TURN);
     }
 
-    @Override
-    public AcolyteAction copy() {
-        AcolyteAction sa = new AcolyteAction();
-        sa.tokenID = tokenID;
-        return sa;
+    public AcolyteAction(int acolyteComponentID) {
+        super(acolyteComponentID, END_TURN);
+        this.villagerID = 0;
+    }
+
+    public AcolyteAction(int acolyteComponentID, int villagerID) {
+        super(acolyteComponentID, END_TURN);
+        this.villagerID = villagerID;
     }
 
     @Override
-    public boolean canExecute(DescentGameState gs) {
+    public List<AbstractAction> _computeAvailableActions(AbstractGameState state) {
+        List<AbstractAction> actions = new ArrayList<>();
+        DescentGameState dgs = (DescentGameState) state;
+        DToken acolyte = (DToken) dgs.getComponentById(tokenID);
+        int heroIdx = acolyte.getOwnerId();
+        if (heroIdx == dgs.getActingFigure().getComponentID()) {
+            HashSet<DToken> adjacentVillagers = new HashSet<>();
+            Vector2D loc = ((Hero) dgs.getComponentById(heroIdx)).getPosition();
+            DescentGridBoard board = dgs.getMasterBoard();
+            List<Vector2D> neighbours = getNeighbourhood(loc.getX(), loc.getY(), board.getWidth(), board.getHeight(), true);
+            neighbours.add(loc);
+            for (DToken token : dgs.getTokens()) {
+                if (token.getDescentTokenType() == DescentTypes.DescentToken.Villager && neighbours.contains(token.getPosition())) {
+                    adjacentVillagers.add(token);
+                }
+            }
+            if (adjacentVillagers.size() > 0) {
+                for (DToken token : adjacentVillagers) {
+                    AcolyteAction newAA = new AcolyteAction(tokenID, token.getComponentID());
+                    if (newAA.canExecute(dgs)) actions.add(newAA);
+                }
+                AcolyteAction newAA = new AcolyteAction(tokenID, -1);
+                if (newAA.canExecute(dgs)) actions.add(newAA);
+            }
+        }
+        if (actions.isEmpty()) actions.add(new DoNothing());
+        return actions;
+    }
+
+    @Override
+    public int getCurrentPlayer(AbstractGameState state) {
+        int figureIdx = state.getComponentById(tokenID).getOwnerId();
+        return state.getComponentById(figureIdx).getOwnerId();
+    }
+
+    @Override
+    public void _afterAction(AbstractGameState state, AbstractAction action) {
+        complete = true;
+    }
+
+    @Override
+    public boolean executionComplete(AbstractGameState state) {
+        return complete;
+    }
+
+    @Override
+    public AcolyteAction copy() {
+        AcolyteAction acolyteAction = new AcolyteAction(tokenID);
+        acolyteAction.villagerID = villagerID;
+        return acolyteAction;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), villagerID, complete);
+    }
+
+    @Override
+    public boolean canExecute(DescentGameState dgs) {
         // Can only execute if player adjacent to villager token
-        DToken acolyte = (DToken) gs.getComponentById(tokenID);
-        Hero hero = gs.getHeroes().get(acolyte.getOwnerId());
-        Vector2D loc = hero.getPosition();
-        DescentGridBoard board = gs.getMasterBoard();
-        List<Vector2D> neighbours = getNeighbourhood(loc.getX(), loc.getY(), board.getWidth(), board.getHeight(), true);
-        for (DToken token: gs.getTokens()) {
-            if (token.getDescentTokenType() == DescentTypes.DescentToken.Villager && neighbours.contains(token.getPosition())) {
-                return true;
+        DToken acolyte = (DToken) dgs.getComponentById(tokenID);
+        int heroIdx = acolyte.getOwnerId();
+        if (heroIdx == dgs.getActingFigure().getComponentID()) {
+            if (villagerID == -1) return true; // Can always skip
+            Hero hero = (Hero) dgs.getComponentById(heroIdx);
+            String lastAction = hero.getActionsTaken().get(hero.getActionsTaken().size() - 1);
+            if (lastAction.contains("Escort Wounded Clergy at")) return false;  // Already escorted one this turn
+            if (lastAction.contains("Skipped Escorting Wounded Clergy")) return false;  // Already skipped this turn
+            Vector2D loc = hero.getPosition();
+            DescentGridBoard board = dgs.getMasterBoard();
+            List<Vector2D> neighbours = getNeighbourhood(loc.getX(), loc.getY(), board.getWidth(), board.getHeight(), true);
+            neighbours.add(loc);
+            if (villagerID != 0) {
+                DToken token = (DToken) dgs.getComponentById(villagerID);
+                return token.getDescentTokenType() == DescentTypes.DescentToken.Villager && neighbours.contains(token.getPosition());
+            }
+            else {
+                for (DToken token: dgs.getTokens()) {
+                    if (token.getDescentTokenType() == DescentTypes.DescentToken.Villager && neighbours.contains(token.getPosition())) {
+                        return true;
+                    }
+                }
             }
         }
         return false;
@@ -51,29 +136,40 @@ public class AcolyteAction extends TokenAction {
 
     @Override
     public boolean equals(Object o) {
-        return super.equals(o) && o instanceof AcolyteAction;
+        return super.equals(o) && o instanceof AcolyteAction && ((AcolyteAction) o).villagerID == villagerID && complete == ((AcolyteAction) o).complete;
     }
 
     @Override
     public String getString(AbstractGameState gameState) {
-        return "Acolyte end turn effect";
+        if (villagerID == 0) return "Acolyte End Turn Effect";
+        if (villagerID == -1) return "Skip Escorting Wounded Clergy";
+        return "Escort Wounded Clergy at " + ((DToken) gameState.getComponentById(villagerID)).getPosition();
     }
 
     @Override
-    public boolean execute(DescentGameState gs) {
-        DToken acolyte = (DToken) gs.getComponentById(tokenID);
-        int heroIdx = acolyte.getOwnerId();
-        Hero hero = gs.getHeroes().get(heroIdx);
-        Vector2D loc = hero.getPosition();
-        DescentGridBoard board = gs.getMasterBoard();
-        List<Vector2D> neighbours = getNeighbourhood(loc.getX(), loc.getY(), board.getWidth(), board.getHeight(), true);
-        for (DToken token: gs.getTokens()) {
-            if (token.getDescentTokenType() == DescentTypes.DescentToken.Villager && neighbours.contains(token.getPosition())) {
-                token.setOwnerId(heroIdx, gs); // Take this one
-                token.setPosition(null);  // Take off the map
+    public boolean execute(DescentGameState dgs) {
+        if (villagerID != 0) {
+            DToken acolyte = (DToken) dgs.getComponentById(tokenID);
+            int heroIdx = acolyte.getOwnerId();
+            Hero hero = (Hero) dgs.getComponentById(heroIdx);
+            if (villagerID == -1) {
+                hero.addActionTaken("Skipped Escorting Wounded Clergy");
                 return true;
             }
+            Vector2D loc = hero.getPosition();
+            DescentGridBoard board = dgs.getMasterBoard();
+            List<Vector2D> neighbours = getNeighbourhood(loc.getX(), loc.getY(), board.getWidth(), board.getHeight(), true);
+            neighbours.add(loc);
+            DToken token = (DToken) dgs.getComponentById(villagerID);
+            if (token.getDescentTokenType() == DescentTypes.DescentToken.Villager && neighbours.contains(token.getPosition())) {
+                token.setOwnerId(heroIdx, dgs); // Take this one
+                token.setPosition(null);  // Take off the map
+                hero.addActionTaken("Escort Wounded Clergy at " + token.getPosition());
+            }
         }
-        return false;
+        else {
+            dgs.setActionInProgress(this);
+        }
+        return true;
     }
 }
