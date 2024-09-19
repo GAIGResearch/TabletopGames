f03269403e1d17de2b279964368ec393ee66efdc
hopshackle <james@janigo.co.uk>
2024-09-19 14:56:24 +0100

    OSLAPlayer resurrected for Game AI course

--- src/main/java/players/simple/OSLAPlayer.java
+++ src/main/java/players/simple/OSLAPlayer.java
@@ -1,34 +1,73 @@
 package players.simple;
 
+import core.AbstractGameState;
+import core.AbstractPlayer;
+import core.actions.AbstractAction;
 import core.interfaces.IStateHeuristic;
-import players.heuristics.GameDefaultHeuristic;
-import players.search.MaxNSearchParameters;
-import players.search.MaxNSearchPlayer;
 
+import java.util.List;
+import java.util.Random;
 
-public class OSLAPlayer extends MaxNSearchPlayer {
+import static utilities.Utils.noise;
 
+public class OSLAPlayer extends AbstractPlayer {
+
+    // Heuristic used for the agent
+    IStateHeuristic heuristic;
+
+    public OSLAPlayer(Random random) {
+        super(null, "SuperOSLA");
+        this.rnd = random;
+    }
 
     public OSLAPlayer() {
-        this(new GameDefaultHeuristic());
+        this(new Random());
     }
 
     public OSLAPlayer(IStateHeuristic heuristic) {
-        super(constructParameters(heuristic));
+        this(heuristic, new Random());
+    }
+
+    public OSLAPlayer(IStateHeuristic heuristic, Random random) {
+        this(random);
+        this.heuristic = heuristic;
         setName("OSLA");
     }
 
-    private static MaxNSearchParameters constructParameters(IStateHeuristic heuristic) {
-        MaxNSearchParameters params = new MaxNSearchParameters();
-        params.setParameterValue("BUDGET_TYPE", "BUDGET_TIME");
-        params.setParameterValue("BUDGET", 100); // 100ms as timeout
-        params.setParameterValue("searchDepth", 1);
-        params.setParameterValue("searchUnit", "ACTION");
-        if (heuristic != null)
-            params.setParameterValue("heuristic", heuristic);
-        else
-            params.setParameterValue("heuristic", new GameDefaultHeuristic());
-        return params;
+    @Override
+    public AbstractAction _getAction(AbstractGameState gs, List<AbstractAction> actions) {
+        double maxQ = Double.NEGATIVE_INFINITY;
+        AbstractAction bestAction = null;
+        double[] valState = new double[actions.size()];
+        int playerID = gs.getCurrentPlayer();
+
+        for (int actionIndex = 0; actionIndex < actions.size(); actionIndex++) {
+            AbstractAction action = actions.get(actionIndex);
+            AbstractGameState gsCopy = gs.copy();
+            getForwardModel().next(gsCopy, action);
+
+            if (heuristic != null) {
+                valState[actionIndex] = heuristic.evaluateState(gsCopy, playerID);
+            } else {
+                valState[actionIndex] = gsCopy.getHeuristicScore(playerID);
+            }
+
+            double Q = noise(valState[actionIndex], getParameters().noiseEpsilon, rnd.nextDouble());
+
+            if (Q > maxQ) {
+                maxQ = Q;
+                bestAction = action;
+            }
+        }
+
+        return bestAction;
+    }
+
+    @Override
+    public OSLAPlayer copy() {
+        OSLAPlayer retValue = new OSLAPlayer(heuristic, new Random(rnd.nextInt()));
+        retValue.setForwardModel(getForwardModel().copy());
+        return retValue;
     }
 
 }
