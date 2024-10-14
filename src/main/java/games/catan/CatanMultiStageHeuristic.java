905666ada2d28e3065e64578f0492507aa0f02cd
hopshackle <james@janigo.co.uk>
2024-10-14 14:57:07 +0100

    CatanMultiStageHeuristic just needs coeff files

--- src/main/java/games/catan/CatanMultiStageHeuristic.java
+++ src/main/java/games/catan/CatanMultiStageHeuristic.java
@@ -4,12 +4,14 @@ import core.AbstractGameState;
 import core.AbstractParameters;
 import core.interfaces.IStateHeuristic;
 import evaluation.optimisation.TunableParameters;
+import players.heuristics.LinearStateHeuristic;
+import players.heuristics.ScoreHeuristic;
 
 import java.util.Arrays;
 
 public class CatanMultiStageHeuristic extends TunableParameters implements IStateHeuristic {
 
-    IStateHeuristic base, opening, early, late;
+    LinearStateHeuristic base, opening, early, late;
     CatanStateFeatures features = new CatanStateFeatures();
     int openingIndex = 35;
     int earlyIndex = 36;
@@ -20,10 +22,13 @@ public class CatanMultiStageHeuristic extends TunableParameters implements IStat
      * This is a state value heuristic that uses a different function for each round
      */
     public CatanMultiStageHeuristic() {
-        addTunableParameter("base", IStateHeuristic.class);
-        addTunableParameter("opening", IStateHeuristic.class);
-        addTunableParameter("early", IStateHeuristic.class);
-        addTunableParameter("late", IStateHeuristic.class);
+        // This is hardcoded for the moment to always use a linear state heuristic for a Score Plus based heuristic
+        // the parameters then define the different coefficient files to use at different stages of the game
+
+        addTunableParameter("base", "");
+        addTunableParameter("opening", "");
+        addTunableParameter("early", "");
+        addTunableParameter("late", "");
         if (!features.localNames[openingIndex].equals("OPENING_GAME")) {
             throw new AssertionError("Opening feature must be at index 35");
         }
@@ -74,13 +79,24 @@ public class CatanMultiStageHeuristic extends TunableParameters implements IStat
 
     @Override
     public void _reset() {
-        base = (IStateHeuristic) getParameterValue("base");
-        opening = (IStateHeuristic) getParameterValue("opening");
-        early = (IStateHeuristic) getParameterValue("early");
-        late = (IStateHeuristic) getParameterValue("late");
-        if (opening == null) opening = base;
-        if (early == null) early = base;
-        if (late == null) late = base;
-        if (opening == null || early == null || late == null) throw new AssertionError("At least base heuristic must be provided");
+        String baseCoeff = (String) getParameterValue("base");
+        String openingCoeff = (String) getParameterValue("opening");
+        String earlyCoeff = (String) getParameterValue("early");
+        String lateCoeff = (String) getParameterValue("late");
+        base = new LinearStateHeuristic(features, baseCoeff, new ScoreHeuristic());
+        if (openingCoeff.isEmpty())
+            opening = base;
+        else
+            opening = new LinearStateHeuristic(features, openingCoeff, new ScoreHeuristic());
+        if (earlyCoeff.isEmpty())
+            early = base;
+        else
+            early = new LinearStateHeuristic(features, earlyCoeff, new ScoreHeuristic());
+        if (lateCoeff.isEmpty())
+            late = base;
+        else
+            late = new LinearStateHeuristic(features, lateCoeff, new ScoreHeuristic());
+        if (opening == null || early == null || late == null)
+            throw new AssertionError("At least base heuristic must be provided");
     }
 }
