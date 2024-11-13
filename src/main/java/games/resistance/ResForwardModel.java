ab02cdcfb550f79f1c044954057002d52158a127
hposhackle <james@janigo.co.uk>
2024-11-13 16:54:13 +0000

    Puerto Rico javadoc

--- src/main/java/games/resistance/ResForwardModel.java
+++ src/main/java/games/resistance/ResForwardModel.java
@@ -270,7 +270,6 @@ public class ResForwardModel extends StandardForwardModel {
         resgs.leaderID = (resgs.leaderID + 1) % resgs.getNPlayers();
     }
 
-
     public static List<Boolean> randomiseSpies(int spies, ResGameState state, int playerID, Random rnd) {
         // We want to randomly assign the number of spies across the total number of players
         // and return a boolean[] with length of total, and spies number of true values
