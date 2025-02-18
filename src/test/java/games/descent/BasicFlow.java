5ad16bc234bf06c7c8c0fee6c16ba385c7e26b72
hopshackle <james@janigo.co.uk>
2025-02-18 20:37:41 +0000

    Test for removing Monster from turn order

--- src/test/java/games/descent/BasicFlow.java
+++ src/test/java/games/descent/BasicFlow.java
@@ -13,6 +13,7 @@ import org.junit.Test;
 import java.util.List;
 import java.util.Random;
 
+import static games.descent2e.DescentHelper.figureDeath;
 import static org.junit.Assert.*;
 
 public class BasicFlow {
@@ -99,6 +100,81 @@ public class BasicFlow {
     public void testMonsterRemoved() {
         // we run through checking that:
         // A monster is removed from the board when it is defeated (and does not take its turn)
+        // we run through checking that:
+        // Each hero has all their actions in turn first
+        // then We move through the monsters in order before the round ends
+        int lastPlayer, currentPlayer;
+        Figure lastFigure, actingFigure;
+        int countOfFigures = 0;
+        do {
+            currentPlayer = state.getCurrentPlayer();
+            actingFigure = state.getActingFigure();
+
+            List<AbstractAction> actions = fm.computeAvailableActions(state);
+            AbstractAction action = actions.get(rnd.nextInt(actions.size()));
+            System.out.println("Player " + currentPlayer + " - " + actingFigure + " - " + action.toString());
+            fm.next(state, action);
+
+            lastPlayer = currentPlayer;
+            lastFigure = actingFigure;
+            currentPlayer = state.getCurrentPlayer();
+            actingFigure = state.getActingFigure();
+            if (actingFigure.getComponentID() != lastFigure.getComponentID()) {
+                countOfFigures++;
+                assertNotEquals(lastFigure, actingFigure);
+                assertNotEquals(lastFigure.getComponentID(), actingFigure.getComponentID());
+                switch (countOfFigures) {
+                    case 1:
+                        assertEquals(1, lastPlayer);
+                        assertEquals(1, currentPlayer);
+                        assertTrue(lastFigure instanceof Hero);
+                        assertTrue(actingFigure instanceof Hero);
+                        // Here we now kill off the goblin master
+                        figureDeath(state, state.getMonsters().get(0).get(0));
+                        assertEquals(0, state.getTurnCounter());
+                        break;
+                    case 2:
+                        assertEquals(1, lastPlayer);
+                        assertEquals(0, currentPlayer);
+                        assertTrue(lastFigure instanceof Hero);
+                        assertTrue(actingFigure instanceof Monster);
+                        assertEquals(1, state.getTurnCounter());
+                        assertEquals(0, state.getRoundCounter());
+                        assertTrue(actingFigure.getName().contains("Goblin"));
+                        break;
+                    case 3: // goblins
+                        assertEquals(0, lastPlayer);
+                        assertEquals(0, currentPlayer);
+                        assertTrue(lastFigure instanceof Monster);
+                        assertTrue(actingFigure instanceof Monster);
+                        assertEquals(1, state.getTurnCounter());
+                        assertEquals(0, state.getRoundCounter());
+                        assertTrue(actingFigure.getName().contains("Goblin"));
+                        break;
+                    case 4, 5: // Barghests
+                        assertEquals(0, lastPlayer);
+                        assertEquals(0, currentPlayer);
+                        assertTrue(lastFigure instanceof Monster);
+                        assertTrue(actingFigure instanceof Monster);
+                        assertEquals(1, state.getTurnCounter());
+                        assertEquals(0, state.getRoundCounter());
+                        assertTrue(actingFigure.getName().contains("Barghest"));
+                        break;
+                    case 6:
+                        assertEquals(0, lastPlayer);
+                        assertEquals(1, currentPlayer);
+                        assertTrue(lastFigure instanceof Monster);
+                        assertTrue(actingFigure instanceof Hero);
+                        assertEquals(0, state.getTurnCounter());
+                        assertEquals(1, state.getRoundCounter());
+                        assertTrue(actingFigure.getName().contains("Hero"));
+                        break;
+                    default:
+                        throw new AssertionError("Too many figures in the game");
+                }
+            }
+
+        } while (state.getRoundCounter() == 0);
     }
 
 }
