e3e03fc976dc39e381f491f1757eea158e1a7f99
hopshackle <james@janigo.co.uk>
2025-03-08 11:50:21 +0000

    Consider combination settings

--- src/main/java/evaluation/optimisation/OneStepDeviations.java
+++ src/main/java/evaluation/optimisation/OneStepDeviations.java
@@ -62,11 +62,12 @@ public class OneStepDeviations {
     public int[] run(int[] baseSettings) {
 
         int[] defaultSettings = searchSpace.defaultSettings();
-        List<int[]> deviations = new ArrayList<>();
+        List<int[]> playerSettings = new ArrayList<>();
 
+        // we store the players with their index in the list, as this makes it easier later when we filter the list by various criteria
         List<Pair<Integer, AbstractPlayer>> players = new ArrayList<>();
         players.add(Pair.of(0, (AbstractPlayer) searchSpace.instantiate(baseSettings)));
-        deviations.add(new int[2]); // dummy for baseline agent
+        playerSettings.add(baseSettings); // dummy for baseline agent
         int nextIndex = 1;
         for (int i = 0; i < baseSettings.length; i++) {
             for (int j = 0; j < params.searchSpace.nValues(i); j++) {
@@ -79,18 +80,25 @@ public class OneStepDeviations {
                 // we set the name to indicate the one step deviation
                 player.setName(String.format("%s: %s", searchSpace.name(i), searchSpace.allValues(i).get(j)));
                 players.add(Pair.of(nextIndex, player));
-                deviations.add(new int[]{i, j});  // the dimension and value of the one step deviation
+                playerSettings.add(settings);
                 nextIndex++;
             }
         }
-        double[] gamesPlayed = new double[players.size()];
-        double[] totalScore = new double[players.size()];
-        double[] totalScoreSquared = new double[players.size()];
+
+        // we track the stats for each player (these need to be lists, as we'll be adding new players as we proceed)
+        List<Integer> gamesPlayed = new ArrayList<>(players.size());
+        List<Double> totalScore = new ArrayList<>(players.size());
+        List<Double> totalScoreSquared = new ArrayList<>(players.size());
+        for (int i = 0; i < players.size(); i++) {
+            gamesPlayed.add(0);
+            totalScore.add(0.0);
+            totalScoreSquared.add(0.0);
+        }
+        List<Integer> agentsMuchBetterThanBaseline = new ArrayList<>();
         boolean finished;
         int iteration = 1;
 
         do {
-
             System.out.printf("Running OSD tournament with remaining players: %d%n", players.size());
 
             // We now have all agents, with [0] being the baseline agent
@@ -115,43 +123,108 @@ public class OneStepDeviations {
             int bestIndex = 0;
             double bestScore = -Double.MAX_VALUE;
             double bestStdError = 0.0;
+            int robustGamesThreshold = iteration * params.tournamentGames / gamesPlayed.size();
             for (int loop = 0; loop < players.size(); loop++) {
                 int i = players.get(loop).a;
-                gamesPlayed[i] += tournament.getNGamesPlayed()[loop];
-                totalScore[i] += params.evalMethod.equals("Win")
-                        ? tournament.getWinRate(loop) * tournament.getNGamesPlayed()[loop]
-                        : -tournament.getOrdinalRank(loop) * tournament.getNGamesPlayed()[loop];
-                totalScoreSquared[i] += tournament.getSumOfSquares(loop, params.evalMethod);
-                if (totalScore[i] / gamesPlayed[i] > bestScore) {
+                int g = tournament.getNGamesPlayed()[loop];
+                gamesPlayed.set(i, gamesPlayed.get(i) + g);
+                totalScore.set(i, totalScore.get(i) + (params.evalMethod.equals("Win")
+                        ? tournament.getWinRate(loop) * g
+                        : -tournament.getOrdinalRank(loop) * g));
+                totalScoreSquared.set(i, totalScoreSquared.get(i) + tournament.getSumOfSquares(loop, params.evalMethod));
+                // if we have played enough games, then check if best
+                if (gamesPlayed.get(i) >= robustGamesThreshold && totalScore.get(i) / gamesPlayed.get(i) > bestScore) {
                     bestIndex = loop;
-                    bestScore = totalScore[i] / gamesPlayed[i];
-                    bestStdError = Math.sqrt(totalScoreSquared[i] / gamesPlayed[i] - Math.pow(totalScore[i] / gamesPlayed[i], 2))
-                            / Math.sqrt(gamesPlayed[i]);
+                    bestScore = totalScore.get(i) / gamesPlayed.get(i);
+                    bestStdError = Math.sqrt(totalScoreSquared.get(i) / gamesPlayed.get(i) - Math.pow(bestScore, 2))
+                            / Math.sqrt(gamesPlayed.get(i));
                 }
             }
 
-            System.out.printf("Iteration %d: Best agent is %s (%d) with score %.3f +/- %.3f%n",
-                    iteration, players.get(bestIndex), bestIndex, bestScore, bestStdError);
+            double baseAgentScore = totalScore.get(0) / gamesPlayed.get(0);
+            System.out.printf("Iteration %d: Best robust agent is %s with score %.3f +/- %.3f%n",
+                    iteration, players.get(bestIndex), bestScore, bestStdError);
+            System.out.printf("Baseline agent has score %.3f%n", baseAgentScore);
             // Now run through all players, and discard any that are significantly worse than the best
             List<Pair<Integer, AbstractPlayer>> newPlayers = new ArrayList<>();
+            double stdErrorMultiple = Utils.standardZScore(0.01, players.size() - 1);
+            double stdBetterMultiple = Utils.standardZScore(0.05, players.size() - 1);
+            System.out.printf("Discarding agents with score more than %.3f standard errors worse than best%n", stdErrorMultiple);
             for (Pair<Integer, AbstractPlayer> player : players) {
                 int i = player.a;
                 if (i == 0 || i == bestIndex) {
                     newPlayers.add(player); // never discard the baseline agent, or the best agent
                     continue;
                 }
-                double stdError = Utils.meanDiffStandardError(totalScore[bestIndex], totalScore[i],
-                        totalScoreSquared[bestIndex], totalScoreSquared[i], (int) gamesPlayed[bestIndex], (int) gamesPlayed[i]);
-                double stdErrorMultiple = Utils.standardZScore(0.01, players.size() - 1);
+                double stdError = Utils.meanDiffStandardError(totalScore.get(bestIndex), totalScore.get(i),
+                        totalScoreSquared.get(bestIndex), totalScoreSquared.get(i), gamesPlayed.get(bestIndex), gamesPlayed.get(i));
                 // We go for a 99% confidence that any given agent is worse than the best (adjusted for multiple comparisons)
                 // This technically does not adjust for the fact we compare against the MAX agent, so implicitly there are N(N-1)/2 comparisons
                 // instead of N-1, but the full set of comparisons are not independent. We adjust for this by choosing a 1% confidence interval.
-                if (totalScore[i] / gamesPlayed[i] > bestScore - stdErrorMultiple * stdError) {
+                if (totalScore.get(i) / gamesPlayed.get(i) >= bestScore - stdErrorMultiple * stdError) {
                     newPlayers.add(Pair.of(i, player.b));
-                    //     System.out.printf("Retaining %s (%d) with score %.3f +/- %.3f %n", players.get(i), i, totalScore[i] / gamesPlayed[i], stdError);
+                    // and also check for significant improvement over the baseline agent
+                    if (!agentsMuchBetterThanBaseline.contains(i)) {
+                        double stdErrorToBase = Utils.meanDiffStandardError(totalScore.get(0), totalScore.get(i),
+                                totalScoreSquared.get(0), totalScoreSquared.get(i), gamesPlayed.get(0), gamesPlayed.get(i));
+                        if (totalScore.get(i) / gamesPlayed.get(i) > baseAgentScore + stdBetterMultiple * stdErrorToBase) {
+                            System.out.printf("Agent %s is significantly better than baseline with score %.3f, and mean diff of %.3f +/- %.3f %n",
+                                    player.b, totalScore.get(i) / gamesPlayed.get(i), totalScore.get(i) / gamesPlayed.get(i) - baseAgentScore, stdErrorToBase);
+                            agentsMuchBetterThanBaseline.add(i);
+                        }
+                    }
                 } else {
                     System.out.printf("Discarding %s (%d) with score %.3f, and mean diff of %.3f +/- %.3f %n",
-                            player.b, i, totalScore[i] / gamesPlayed[i], bestScore - totalScore[i] / gamesPlayed[i], stdError);
+                            player.b, i, totalScore.get(i) / gamesPlayed.get(i), bestScore - totalScore.get(i) / gamesPlayed.get(i), stdError);
+                }
+            }
+            // TODO: Now add in new combo-players that combine the setting deviations of players that are better then baseline
+            // We use all compatible combinations of agentsMuchBetterThanBaseline, and add them to the list of players
+            // after checking that these settings have not already been tried
+            // A 'compatible' combination is one where the deviation indices do not overlap
+
+            // First all possible combinations of agentsMuchBetterThanBaseline
+            for (int i = 0; i < agentsMuchBetterThanBaseline.size(); i++) {
+                for (int j = i + 1; j < agentsMuchBetterThanBaseline.size(); j++) {
+                    int[] settings1 = playerSettings.get(agentsMuchBetterThanBaseline.get(i));
+                    int[] settings2 = playerSettings.get(agentsMuchBetterThanBaseline.get(j));
+                    boolean compatible = true;
+                    for (int k = 0; k < settings1.length; k++) {
+                        if (settings1[k] != baseSettings[k] && settings2[k] != baseSettings[k]) {
+                            compatible = false;
+                        }
+                    }
+                    if (compatible) {
+                        int[] combinedSettings = settings1.clone();
+                        // use 1 as the base, and add in the changes that 2 makes
+                        for (int k = 0; k < settings1.length; k++) {
+                            if (settings2[k] != baseSettings[k]) {
+                                combinedSettings[k] = settings2[k];
+                            }
+                        }
+                        // check for duplicate
+                        if (!playerSettings.contains(combinedSettings)) {
+                            // add new player
+                            AbstractPlayer player = (AbstractPlayer) searchSpace.instantiate(combinedSettings);
+                            int finalI = i;
+                            int iIndex = players.stream()
+                                    .filter(data -> data.a.equals(agentsMuchBetterThanBaseline.get(finalI)))
+                                            .findFirst().get().a;
+                            int finalJ = j;
+                            int jIndex = players.stream()
+                                    .filter(data -> data.a.equals(agentsMuchBetterThanBaseline.get(finalJ)))
+                                            .findFirst().get().a;
+                            player.setName(String.format("%s + %s", players.get(iIndex).b,
+                                    players.get(jIndex).b));
+                            newPlayers.add(Pair.of(nextIndex, player));
+                            playerSettings.add(combinedSettings);
+                            gamesPlayed.add(0);
+                            totalScore.add(0.0);
+                            totalScoreSquared.add(0.0);
+                            nextIndex++;
+                            System.out.printf("Adding new player %s with settings %s%n", player, Arrays.toString(combinedSettings));
+                        }
+                    }
                 }
             }
 
@@ -162,58 +235,35 @@ public class OneStepDeviations {
         } while (!finished);
 
         // Now we work out the final tweaked 'best' agent
-        int[] tweakedSettings = baseSettings.clone();
-        double baseAgentScore = totalScore[0] / gamesPlayed[0];
-        System.out.printf("Base agent score is %.3f%n", baseAgentScore);
-        for (int dimension = 0; dimension < baseSettings.length; dimension++) {
-            // firstly extract all the players that affect this dimension, and sort them in descending order of score
-            int finalDimension = dimension;
-            List<Pair<Integer, AbstractPlayer>> dimensionPlayers = players.stream()
-                    .filter(p -> p.a > 0)  // ignore the baseline agent
-                    .filter(p -> deviations.get(p.a)[0] == finalDimension)
-                    .sorted(Comparator.comparingDouble(p -> -totalScore[p.a] / gamesPlayed[p.a]))
-                    .toList();
-            if (dimensionPlayers.isEmpty()) {
-                System.out.printf("No players for dimension %s, keeping base value%n", searchSpace.name(dimension));
-                continue;  // no change to baseline
-            }
-            for (Pair<Integer, AbstractPlayer> stuff : dimensionPlayers) {
-                int[] settings = deviations.get(stuff.a);
-                if (settings[0] != dimension) {
-                    throw new AssertionError("Dimension mismatch");
-                }
-                double score = totalScore[stuff.a] / gamesPlayed[stuff.a];
-                double stdError = Utils.meanDiffStandardError(totalScore[0], totalScore[stuff.a],
-                        totalScoreSquared[0], totalScoreSquared[stuff.a], (int) gamesPlayed[0], (int) gamesPlayed[stuff.a]);
-                double adjustedZScore = Utils.standardZScore(0.05, players.size() - 1);
-                System.out.printf("Processing Agent %s (%d) with score %.3f, diff to base of %.3f +/- %.3f %n",
-                        stuff.b, stuff.a, score, score - baseAgentScore, stdError);
-                double adjustedZDefaultComparison = Utils.standardZScore(0.25, baseSettings.length);
-                if (score > baseAgentScore + adjustedZScore * stdError) {
-                    System.out.printf("Tweaking %s to %s with score %.3f, better than base agent by %.3f +/- %.3f%n",
-                            searchSpace.name(dimension), searchSpace.allValues(dimension).get(settings[1]), score,
-                            score - baseAgentScore, stdError);
-                    tweakedSettings[dimension] = settings[1];
-                    break;
-                } else if (score > baseAgentScore - adjustedZDefaultComparison * stdError) {
-                    if (settings[1] == defaultSettings[dimension]) {
-                        double bestScore = totalScore[dimensionPlayers.get(0).a] / gamesPlayed[dimensionPlayers.get(0).a];
-                        // add an additional check that the default agent is also within spitting distance of the best agent
-                        // to avoid cases where we set to the default even though this is quite a bit worse than the best agent on this setting
-                        // (in the worst case this could be 2.99 standard deviations worse than the best...so sticking to the tuned value is a better idea)
-                        if (score > bestScore - adjustedZDefaultComparison * stdError) {
-                            System.out.printf("Setting to default %s (%s) with score %.3f, improving over base agent by %.3f +/- %.3f%n",
-                                    searchSpace.name(dimension), searchSpace.allValues(dimension).get(settings[1]), score,
-                                    score - baseAgentScore, stdError);
-                            tweakedSettings[dimension] = settings[1];
-                            break;
-                        }
-                    }
-                } else {
-                    break;  // we have reached the worse agents
-                }
+        // We take the single best agent that is better than the baseline
+        // As long as this improvement is significant. Keep working through all agents, but now track the best lower bound
+        // TODO: If all the deviations are to their default values, then we are more lenient in the significance test
+        double baseAgentScore = totalScore.get(0) / gamesPlayed.get(0);
+        double adjustedZScore = Utils.standardZScore(0.05, players.size() - 1);
+        double bestLowerBound = baseAgentScore;
+        int selectedPlayer = -1;
+        System.out.printf("Base agent score is %.3f, and zScore needed is %.3f%n", baseAgentScore, adjustedZScore);
+        for (Pair<Integer, AbstractPlayer> stuff : players) {
+            int playerIndex = stuff.a;
+            double score = totalScore.get(playerIndex) / gamesPlayed.get(playerIndex);
+            double stdError = Utils.meanDiffStandardError(totalScore.get(0), totalScore.get(playerIndex),
+                    totalScoreSquared.get(0), totalScoreSquared.get(playerIndex), gamesPlayed.get(0), gamesPlayed.get(playerIndex));
+            System.out.printf("Processing Agent %s (%d) with score %.3f, diff to base of %.3f +/- %.3f %n",
+                    stuff.b, stuff.a, score, score - baseAgentScore, stdError);
+            if (score > baseAgentScore + adjustedZScore * stdError && score - adjustedZScore * stdError > bestLowerBound) {
+                // best so far
+                bestLowerBound = score - adjustedZScore * stdError;
+                selectedPlayer = playerIndex;
             }
         }
-        return tweakedSettings;
+        if (selectedPlayer == -1) {
+            System.out.println("No significant improvement found");
+            return baseSettings;
+        } else {
+            int finalSelectedPlayer = selectedPlayer;
+            AbstractPlayer playerSelected = players.stream().filter(data -> data.a == finalSelectedPlayer).findFirst().get().b;
+            System.out.printf("Selected agent is %s with score %.3f%n", playerSelected, totalScore.get(selectedPlayer) / gamesPlayed.get(selectedPlayer));
+            return playerSettings.get(selectedPlayer);
+        }
     }
 }
