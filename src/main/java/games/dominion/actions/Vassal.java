343c09ec8e2a439dddac087b07e6a100f0c85df3
George <george@longhouse.dev>
2025-04-24 16:55:17 +0100

    Why turning null?

--- src/main/java/games/dominion/actions/Vassal.java
+++ src/main/java/games/dominion/actions/Vassal.java
@@ -18,6 +18,8 @@ import games.dominion.cards.DominionCard;
 public class Vassal extends DominionAction implements IExtendedSequence {
 
     boolean executed = false;
+    boolean emptyDeck = false;
+    DominionCard drawnCard;
 
     public Vassal(int playerId) {
         super(CardType.VASSAL, playerId);
@@ -37,6 +39,8 @@ public class Vassal extends DominionAction implements IExtendedSequence {
     public Vassal copy() {
         Vassal retValue = new Vassal(player, dummyAction);
         retValue.executed = executed;
+        retValue.emptyDeck = emptyDeck;
+        retValue.drawnCard = drawnCard;
         return retValue;
     }
 
@@ -48,14 +52,15 @@ public class Vassal extends DominionAction implements IExtendedSequence {
         actions.add(new DoNothing());
 
         if (dgs.getDeck(DeckType.DRAW, player).getSize() == 0) {
+            emptyDeck = true;
             return actions;
         }
 
         // Draw the top card of the deck, and if action card, it can be played
         // This card is discarded afterwards
-        DominionCard card = dgs.getDeck(DeckType.DRAW, player).draw();
-        if (card.isActionCard()) {
-            DominionAction action = card.getAction(player);
+        drawnCard = dgs.getDeck(DeckType.DRAW, player).draw().copy();
+        if (drawnCard.isActionCard()) {
+            DominionAction action = drawnCard.getAction(player);
             action.dummyAction = true;
             actions.add(action);
         }
@@ -70,12 +75,53 @@ public class Vassal extends DominionAction implements IExtendedSequence {
 
     @Override
     public void _afterAction(AbstractGameState state, AbstractAction action) {
+        DominionGameState dgs = (DominionGameState) state;
         executed = true;
+
+        if (emptyDeck) {
+            return;
+        }
+        else if (action instanceof DoNothing) {
+            dgs.getDeck(DeckType.DISCARD, player).add(drawnCard);
+        }
+        else {
+            dgs.getDeck(DeckType.TABLE, player).add(drawnCard);
+        }
     }
 
     @Override
     public boolean executionComplete(AbstractGameState state) {
         return executed;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Vassal) {
+            Vassal other = (Vassal) obj;
+
+            boolean sameCard;
+
+            if (drawnCard == null && other.drawnCard == null) {
+                sameCard = true;
+            }
+            else if (drawnCard != null && drawnCard.equals(other.drawnCard)) {
+                sameCard = true;
+            }
+            else {
+                sameCard = false;
+            }
+
+            return super.equals(obj) && other.executed == executed
+                                     && emptyDeck == other.emptyDeck
+                                     && sameCard;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int cardHash = drawnCard != null ? drawnCard.hashCode() : 0;
+        return super.hashCode() + (executed ? 1 : 0) + cardHash + (emptyDeck ? 1 : 0);
     }
     
 }
