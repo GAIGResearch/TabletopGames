69bbbe22468c9fe7697eed7c8c33c4772c790adf
hopshackle <james@janigo.co.uk>
2025-06-25 17:27:15 +0100

    Added first die action feature

--- src/main/java/games/backgammon/BGActionFeatures.java
+++ src/main/java/games/backgammon/BGActionFeatures.java
@@ -10,7 +10,7 @@ public class BGActionFeatures implements IActionFeatureVector {
     String[] names = new String[] {
             "FromPosition", "ToPosition",
             "FromOccupancy", "ToOccupancy",
-            "Blot"
+            "Blot", "FirstDie"
     };
 
     @Override
@@ -38,6 +38,9 @@ public class BGActionFeatures implements IActionFeatureVector {
         // opponent representation is reversed to our, so our position 0 is their position 23
         int opponentID = 1 - playerID;
         features[4] = (move.to >= 0 && bgState.getPiecesOnPoint(opponentID, boardLength - move.to - 1) == 1) ? 1.0 : 0.0;
+
+        // Is this out first die to use?
+        features[5] = bgState.getAvailableDiceValues().length == 2 ? 1.0 : 0.0;
 
         return features;
 
