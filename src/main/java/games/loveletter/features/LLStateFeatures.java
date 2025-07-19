33a63cec57c58c44022b485db7028b5dc7f56baa
hopshackle <james@janigo.co.uk>
2025-07-19 10:10:45 +0100

    Corrected non-tunable version of LLStateFeatures

--- src/main/java/games/loveletter/features/LLStateFeatures.java
+++ src/main/java/games/loveletter/features/LLStateFeatures.java
@@ -8,6 +8,10 @@ public class LLStateFeatures implements IStateFeatureVector {
     LLStateFeaturesTunable underlyingFeatures;
     public LLStateFeatures() {
         this.underlyingFeatures = new LLStateFeaturesTunable();
+        // then set all active features to true
+        for (String name : LLStateFeaturesTunable.allNames) {
+            underlyingFeatures.setParameterValue(name, true);
+        }
     }
 
     @Override
